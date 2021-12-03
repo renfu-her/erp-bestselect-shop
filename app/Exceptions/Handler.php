44d2ac98bb8b8cde0dd2ab103f869e86c1d9cd3d
許志恆 <hayashi0126@gmal.com>
2021-12-03 16:59:21 +0800

    start

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Throwable;
+
+class Handler extends ExceptionHandler
+{
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var string[]
+     */
+    protected $dontReport = [
+        //
+    ];
+
+    /**
+     * A list of the inputs that are never flashed for validation exceptions.
+     *
+     * @var string[]
+     */
+    protected $dontFlash = [
+        'current_password',
+        'password',
+        'password_confirmation',
+    ];
+
+    /**
+     * Register the exception handling callbacks for the application.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->reportable(function (Throwable $e) {
+            //
+        });
+
+        $this->renderable(function (NotFoundHttpException $e, $request) {
+
+            return response()->json(["message" => 404], 404);
+        });
+    }
+
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+
+        $guard = $exception->guards()[0];
+        if ($request->expectsJson()) {
+            return response()->json(['status' => 'A01', 'message' => 'Unauthenticated'], 401);
+        }
+
+        switch ($guard) {
+            default:
+                return redirect(Route('cms.login'));
+        }
+    }
+
+}
---
+++ routes/api.php
@@ -0,0 +1,53 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+ */
+
+Route::get('/tokens/get', function (Request $request) {
+  
+   //  $token = User::where('id', 1)->get()->first()->tokens()->delete();
+   $token = User::where('id', 1)->get()->first()->tokens()->plainTextToken();
+   dd($token);
+    return 'ok';
+});
+
+Route::get('/tokens/create', function (Request $request) {
+    // $token = $request->user()->createToken($request->token_name);
+    // dd($request->token_name);
+    $token = User::where('id', 1)->get()->first()->createToken('ddd');
+    return ['token' => $token->plainTextToken];
+});
+
+Route::group(['prefix' => 'user', 'as' => 'user.', 'middleware' => 'auth:sanctum'], function () {
+    Route::get('/tokens/delete-all', function (Request $request) {
+        // $token = $request->user()->createToken($request->token_name);
+        // dd($request->token_name);
+        $request->user()->tokens()->delete();
+        return 'ok';
+    });
+
+    Route::get('/tokens/delete-current', function (Request $request) {
+        // $token = $request->user()->createToken($request->token_name);
+        // dd($request->token_name);
+        $request->user()->currentAccessToken()->delete();
+        return 'ok';
+    });
+
+    Route::get('/user', function (Request $request) {
+
+        return $request->user();
+    });
+
+});
