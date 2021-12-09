b4f4e128a77be0323bbf8620b33bffa7b30ebaa0
Yoyo <conan0828+gitlab@gmail.com>
2021-12-09 16:38:55 +0800

    add Envoy package and deploy tasks

---
+++ Envoy.blade.php
@@ -0,0 +1,31 @@
+@servers(['localhost' => '127.0.0.1'])
+
+@story('deploy-dev')
+    g-pull-ori
+    setup-pkg-dev
+    reset-db-dev
+    serve
+@endstory
+
+@task('g-pull-ori', ['confirm' => true])
+    @if($branch)
+        git checkout {{ $branch }}
+        git pull origin {{ $branch }}
+    @endif
+@endtask
+
+@task('setup-pkg-dev')
+    rm composer.lock
+    composer install
+    npm run dev
+@endtask
+
+@task('reset-db-dev')
+    php artisan migrate:reset
+    php artisan migrate
+    php artisan db:seed
+@endtask
+
+@task('serve')
+    php artisan serve
+@endtask
