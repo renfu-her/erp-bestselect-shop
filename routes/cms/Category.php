e3335b725a1e7958e9a8ff09f6988165f4d00dcb
Yoyo <conan0828+gitlab@gmail.com>
2021-12-21 10:52:58 +0800

    add permission middleware in Route Group Fix #13

--- routes/cms/Category.php
+++ routes/cms/Category.php
@@ -4,10 +4,10 @@ use App\Http\Controllers\Cms\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'category','as'=>'category.'], function () {
-    Route::get('', [CategoryController::class, 'index'])->name('index');
-    Route::get('edit/{id}', [CategoryController::class, 'edit'])->name('edit');
+    Route::get('', [CategoryController::class, 'index'])->name('index')->middleware('permission:cms.category.index');
+    Route::get('edit/{id}', [CategoryController::class, 'edit'])->name('edit')->middleware('permission:cms.category.edit');
     Route::post('edit/{id}', [CategoryController::class, 'update']);
-    Route::get('create', [CategoryController::class, 'create'])->name('create');
+    Route::get('create', [CategoryController::class, 'create'])->name('create')->middleware('permission:cms.category.create');
     Route::post('create', [CategoryController::class, 'store']);
-    Route::get('delete/{id}', [CategoryController::class, 'destroy'])->name('delete');
+    Route::get('delete/{id}', [CategoryController::class, 'destroy'])->name('delete')->middleware('permission:cms.category.delete');
 });
--- routes/cms/User.php
+++ routes/cms/User.php
@@ -4,10 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Cms\UserCtrl;
 
 Route::group(['prefix' => 'user', 'as' => 'user.'], function () {
-    Route::get('', [UserCtrl::class, 'index'])->name('index');
-    Route::get('create', [UserCtrl::class, 'create'])->name('create');
+    Route::get('', [UserCtrl::class, 'index'])->name('index')->middleware('permission:cms.user.index');
+    Route::get('create', [UserCtrl::class, 'create'])->name('create')->middleware('permission:cms.user.create');
     Route::post('create', [UserCtrl::class, 'store']);
-    Route::get('edit/{id}', [UserCtrl::class, 'edit'])->name('edit');
+    Route::get('edit/{id}', [UserCtrl::class, 'edit'])->name('edit')->middleware('permission:cms.user.edit');
     Route::post('edit/{id}', [UserCtrl::class, 'update']);
-    Route::get('delete/{id}', [UserCtrl::class, 'destroy'])->name('delete');
+    Route::get('delete/{id}', [UserCtrl::class, 'destroy'])->name('delete')->middleware('permission:cms.user.delete');
 });
