74d71e975fde2fc2f3af531aa6cdac0dc1123b62
Yoyo <conan0828+gitlab@gmail.com>
2021-12-21 10:49:18 +0800

    add Spatie Permission and Role test cases

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
@@ -15,11 +16,13 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
+            'account' => $this->faker->firstName(),
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'password' => Hash::make($this->faker->password),
+            'remember_token' => Str::random(80),
+            'uuid' => Str::uuid()
         ];
     }
 
