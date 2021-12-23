d4ed77175677869b9e302470ec0de74b877ede92
08080 <pntcwz@gmail.com>
2021-12-23 14:07:04 +0800

    更新資料表欄位 採購

--- database/migrations/2021_12_21_032235_create_pcs_paying_orders_items_table.php
+++ database/migrations/2021_12_21_032235_create_pcs_paying_orders_items_table.php
@@ -15,10 +15,13 @@ class CreatePcsPayingOrdersItemsTable extends Migration
     {
         Schema::create('pcs_paying_orders', function (Blueprint $table) {
             $table->id()->comment('代墊單id 採購時使用');
+            $table->integer('purchase_id')->comment('採購id');
             $table->tinyInteger('type')->nullable()->comment('代墊單類型 0:訂金 1:尾款');
             $table->string('order_num')->comment('代墊單號');
             $table->string('price')->nullable()->comment('金額');
             $table->string('pay_date')->nullable()->comment('付款日期');
+            $table->timestamps();
+            $table->softDeletes();
         });
     }
 
