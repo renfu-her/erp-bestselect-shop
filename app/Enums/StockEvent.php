20ea5147c98acb04a89c40c541582bf9ebefbdf9
許志恆 <hayashi0126@gmal.com>
2021-12-28 11:52:22 +0800

    stock log done

---
+++ app/Enums/StockEvent.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+/**
+ * @method static static OptionOne()
+ * @method static static OptionTwo()
+ * @method static static OptionThree()
+ */
+final class StockEvent extends Enum
+{
+    const purchase = 'purchase';
+    const order = 'order';
+
+    public static function getDescription($value): string
+    {
+        $result = '';
+        switch ($value) {
+            case self::purchase:
+                $result = '採購';
+                break;
+            case self::order:
+                $result = '訂單';
+                break;
+            default:
+                $result = parent::getDescription($value);
+                break;
+        }
+        return $result;
+    }
+
+}
