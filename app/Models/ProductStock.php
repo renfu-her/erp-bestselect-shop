a9d4bb3f1478e164c01610a2de54832dc1a47032
許志恆 <hayashi0126@gmal.com>
2022-01-06 17:51:56 +0800

    stock model done

--- app/Models/ProductStock.php
+++ app/Models/ProductStock.php
@@ -17,17 +17,14 @@ class ProductStock extends Model
     {
 
         if (!is_numeric($qty)) {
-            DB::rollBack();
             return ['success' => 0, 'error_msg' => 'qty type error'];
         }
 
         if (!StockEvent::hasKey($event)) {
-            DB::rollBack();
             return ['success' => 0, 'error_msg' => 'event error'];
         }
 
         if (!ProductStyle::where('id', $product_style_id)->get()->first()) {
-            DB::rollBack();
             return ['success' => 0, 'error_msg' => 'style not exists'];
         }
 
@@ -38,7 +35,7 @@ class ProductStock extends Model
 
             if (ProductStyle::where('id', $product_style_id)->where('in_stock', '<', 0)->get()->first()) {
                 DB::rollBack();
-                return ['success' => 0, 'error_msg' => 'style overdraft'];
+                return ['success' => 0, 'error_msg' => '數量超出範圍'];
             }
 
             self::create(['product_style_id' => $product_style_id,
@@ -50,5 +47,55 @@ class ProductStock extends Model
             return ['success' => 1];
 
         });
+    }
+
+    public static function comboProcess($style_id, $qty)
+    {
+        return DB::transaction(function () use ($style_id, $qty) {
+
+            $style = ProductStyle::where('id', $style_id)->whereNotNull('sku')->where('type', 'c')->get()->first();
+            if (!$style) {
+                return ['success' => 0, 'error_msg' => '無此商品'];
+            }
+
+            $combos = ProductStyleCombo::where('product_style_id', $style_id)->get()->toArray();
+
+            foreach ($combos as $combo) {
+                $_qty = $qty * -1 * $combo['qty'];
+
+                ProductStyle::where('id', $combo['id'])
+                    ->update(['in_stock' => DB::raw("in_stock + $_qty")]);
+
+                if (ProductStyle::where('id', $combo['id'])->where('in_stock', '<', 0)->get()->first()) {
+                    DB::rollBack();
+                    return ['success' => 0, 'error_msg' => '數量超出範圍'];
+                }
+
+                self::create(['product_style_id' => $combo['id'],
+                    'qty' => $_qty,
+                    'event' => 'combo',
+                    'event_id' => $style_id,
+                    'note' => ($qty < 0) ? '拆包' : '合包']);
+
+            }
+
+            ProductStyle::where('id', $style_id)
+                ->update(['in_stock' => DB::raw("in_stock + $qty")]);
+
+            if (ProductStyle::where('id', $style_id)->where('in_stock', '<', 0)->get()->first()) {
+                DB::rollBack();
+                return ['success' => 0, 'error_msg' => '數量超出範圍'];
+            }
+
+            self::create(['product_style_id' => $style_id,
+                'qty' => $qty,
+                'event' => 'combo',
+                'event_id' => null,
+                'note' => '']);
+
+            return ['success' => 1];
+
+        });
+
     }
 }
