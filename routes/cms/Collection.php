b1b06daf0e5601b4baaa95d71a06bba9ee633fc5
Yoyo <conan0828+gitlab@gmail.com>
2022-01-12 11:08:38 +0800

    Add 群組設定 Menu,Page permission,Route, Controller and Model

---
+++ routes/cms/Collection.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\Cms\Frontend\CollectionCtrl;
+use Illuminate\Support\Facades\Route;
+
+Route::group(['prefix' => 'collection','as'=>'collection.'], function () {
+    Route::get('', [CollectionCtrl::class, 'index'])->name('index')->middleware('permission:cms.collection.index');
+    Route::get('edit/{id}', [CollectionCtrl::class, 'edit'])->name('edit')->middleware('permission:cms.collection.edit');
+    Route::post('edit/{id}', [CollectionCtrl::class, 'update']);
+    Route::get('create', [CollectionCtrl::class, 'create'])->name('create')->middleware('permission:cms.collection.create');
+    Route::post('create', [CollectionCtrl::class, 'store']);
+    Route::get('delete/{id}', [CollectionCtrl::class, 'destroy'])->name('delete')->middleware('permission:cms.collection.delete');
+});
