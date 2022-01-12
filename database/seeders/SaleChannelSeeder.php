c50f475372469d7e6b701b294c69f54cf17126fb
許志恆 <hayashi0126@gmal.com>
2022-01-12 16:23:34 +0800

    model ok  Please enter the commit message for your changes. Lines starting

--- database/seeders/SaleChannelSeeder.php
+++ database/seeders/SaleChannelSeeder.php
@@ -14,6 +14,7 @@ class SaleChannelSeeder extends Seeder
      */
     public function run()
     {
+        
         SaleChannel::create([
             'title' => '喜鴻官網2.0',
             'contact_person' => '喜鴻窗口',
@@ -32,6 +33,8 @@ class SaleChannelSeeder extends Seeder
             'sales_type' => 1,
             'use_coupon' => 1,
         ]);
-
+        
+        SaleChannel::changePrice(1,1,90,120,130,5,10);
+       
     }
 }
