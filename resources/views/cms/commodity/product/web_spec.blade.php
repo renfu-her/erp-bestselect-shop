489d634350241f1271adb00e96007163460e5d90
May.Wang <hsihung08079@gmail.com>
2022-01-13 12:00:41 +0800

    mod title

--- resources/views/cms/commodity/product/web_spec.blade.php
+++ resources/views/cms/commodity/product/web_spec.blade.php
@@ -6,7 +6,7 @@
 </div>
 <form action="">
     <div id="specList" class="card shadow p-4 mb-4">
-        <h6>規格說明（官網）</h6>
+        <h6>規格說明（網頁）</h6>
         <div class="sortabled mb-3 -appendClone">
             <div class="mb-2 row sortabled_box -cloneElem">
                 <div class="col d-flex flex-column flex-sm-row pe-0">
