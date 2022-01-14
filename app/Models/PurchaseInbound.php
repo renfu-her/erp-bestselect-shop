8e034e0551c6e3836c1b7f70d4c225c5c9446423
08080 <pntcwz@gmail.com>
2022-01-14 15:42:53 +0800

    test

--- app/Models/PurchaseInbound.php
+++ app/Models/PurchaseInbound.php
@@ -163,20 +163,23 @@ class PurchaseInbound extends Model
             ->leftJoin('usr_users as users', 'users.id', '=', 'products.user_id')
             ->select('purchase.id as purchase_id' //採購ID
                 , 'items.product_style_id as product_style_id' //商品款式ID
-//                , 'products.title as product_title' //商品名稱
-//                , 'styles.title as style_title' //款式名稱
-//                , 'users.name as user_name' //商品負責人
+                , 'products.title as product_title' //商品名稱
+                , 'styles.title as style_title' //款式名稱
+                , 'users.name as user_name' //商品負責人
             )
             ->selectRaw('min(items.sku) as sku') //款式SKU
             ->selectRaw('sum(items.num) as num') //採購數量
-//            ->selectRaw('(inbound.inbound_num) as inbound_num') //已到數量
-//            ->selectRaw('(inbound.error_num) as error_num') //異常數量
-//            ->selectRaw('( COALESCE(sum(items.num), 0) - COALESCE((inbound.inbound_num), 0) ) AS should_enter_num') //應進數量
+            ->selectRaw('min(inbound.inbound_num) as inbound_num') //已到數量
+            ->selectRaw('min(inbound.error_num) as error_num') //異常數量
+            ->selectRaw('( COALESCE(sum(items.num), 0) - COALESCE((inbound.inbound_num), 0) ) AS should_enter_num') //應進數量
             ->whereNull('purchase.deleted_at')
             ->whereNull('items.deleted_at')
             ->where('purchase.id', '=', $purchase_id)
             ->groupBy('purchase.id'
                 , 'items.product_style_id'
+                , 'products.title'
+                , 'styles.title'
+                , 'users.name'
             )
             ->orderBy('purchase.id')
             ->orderBy('items.product_style_id')
