57508cb564357a192d171a5fb0bc547b029219ab
08080 <pntcwz@gmail.com>
2022-01-22 16:06:59 +0800

    修正 編輯橫幅廣告

---
+++ app/Models/Template.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class Template extends Model
+{
+    use HasFactory;
+    protected $table = 'idx_template';
+    protected $guarded = [];
+
+    public static function storeNew(Request $request)
+    {
+        $request = self::validInputValue($request);
+        return DB::transaction(function () use ($request
+        ) {
+            $id = Template::create([
+                'title' => $request->input('title')
+                , 'group_id' => $request->input('group_id')
+                , 'style_type' => $request->input('style_type')
+            ])->id;
+            return $id;
+        });
+    }
+
+    public static function validInputValue(Request $request) {
+        $request->validate([
+            'title' => 'required|string'
+            , 'group_id' => 'required|numeric'
+            , 'style_type' => 'required|numeric'
+        ]);
+        return $request;
+    }
+
+    public static function updateData(Request $request, int $id)
+    {
+        $request = self::validInputValue($request);
+        $data = Template::where('id', '=', $id);
+        $dataGet = $data->get()->first();
+        return DB::transaction(function () use ($request, $id, $dataGet
+        ) {
+            if (null != $dataGet) {
+                $updateData = [
+                    'title' => $request->input('title')
+                    , 'group_id' => $request->input('group_id')
+                    , 'style_type' => $request->input('style_type')
+                ];
+
+                Template::where('id', '=', $id)
+                    ->update($updateData);
+            }
+
+            return $id;
+        });
+    }
+
+    public static function destroyById(int $id)
+    {
+        Template::destroy($id);
+    }
+
+    public static function sort(Request $request)
+    {
+        $req_template_ids = $request->input('template_id');
+        if (isset($req_template_ids) && 0 < count($req_template_ids)) {
+            $template_ids = implode(',', $req_template_ids);
+            $condtion = '';
+            foreach ($req_template_ids as $sort => $id) {
+                $condtion = $condtion. ' when '. $id. ' then '. $sort;
+            }
+            DB::update('update idx_template set sort = case id'
+                . $condtion
+                . ' end where id in ('. $template_ids. ')'
+            );
+        }
+    }
+
+    public static function getList() {
+        return DB::table('idx_template as template');
+    }
+}
---
+++ database/migrations/2022_01_22_143737_create_idx_template_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIdxTemplateTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('idx_template', function (Blueprint $table) {
+            $table->id()->comment('版型ID');
+            $table->string('title')->comment('大標題');
+            $table->integer('group_id')->nullable()->comment('商品群組id');
+            $table->tinyInteger('style_type')->nullable()->comment('樣式 1:樣式一（左右滑動） 2:... 3:...');
+            $table->integer('sort')->default(100)->comment('排序');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('idx_template');
+    }
+}
