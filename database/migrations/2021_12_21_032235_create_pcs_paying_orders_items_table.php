72e4ccf7a506e7aa8f4202713073fb62926c951b
08080 <pntcwz@gmail.com>
2022-02-09 17:42:51 +0800

    修改欄位 付款單table

--- database/migrations/2021_12_21_032235_create_pcs_paying_orders_items_table.php
+++ database/migrations/2021_12_21_032235_create_pcs_paying_orders_items_table.php
@@ -16,17 +16,21 @@ class CreatePcsPayingOrdersItemsTable extends Migration
         Schema::create('pcs_paying_orders', function (Blueprint $table) {
             $table->id()->comment('代墊單id 採購時使用');
             $table->integer('purchase_id')->comment('採購id');
-            $table->tinyInteger('type')->nullable()->comment('代墊單類型 0:訂金 1:尾款');
+            $table->tinyInteger('type')->comment('代墊單類型 0:訂金 1:尾款');
 
-            $table->string('bank_cname')->comment('匯款銀行');
-            $table->string('bank_code')->comment('匯款銀行代碼');
-            $table->string('bank_acount')->comment('匯款戶名');
-            $table->string('bank_numer')->comment('匯款帳號');
+            $table->string('bank_cname')->nullable()->comment('匯款銀行');
+            $table->string('bank_code')->nullable()->comment('匯款銀行代碼');
+            $table->string('bank_acount')->nullable()->comment('匯款戶名');
+            $table->string('bank_numer')->nullable()->comment('匯款帳號');
+            $table->string('cheque_payable')->nullable()->comment('支票抬頭');
+
+            $table->string('sn')->comment('付款單號');
+            $table->string('price')->nullable()->comment('金額');
             $table->string('logistic_price')->default(0)->comment('物流運費(輸入)(提示無外加運費填0)(預設0)');
 
-            $table->string('sn')->comment('代墊單號');
-            $table->string('price')->nullable()->comment('金額');
+            $table->dateTime('expecte_pay_date')->nullable()->comment('期望付款日期');
             $table->string('pay_date')->nullable()->comment('付款日期');
+            $table->string('summary')->nullable()->comment('摘要');
             $table->string('memo')->nullable()->comment('備註');
             $table->timestamps();
             $table->softDeletes();
