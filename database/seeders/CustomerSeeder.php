89f8ff706ebec6892ca33b5f16b50dcc526379a2
08080 <pntcwz@gmail.com>
2022-02-14 11:51:08 +0800

    新增SEEDER 消費者身分類別

--- database/seeders/CustomerSeeder.php
+++ database/seeders/CustomerSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Enums\Customer\Identity;
 use App\Models\Customer;
+use App\Models\CustomerIdentity;
 use Illuminate\Database\Seeder;
 
 class CustomerSeeder extends Seeder
@@ -14,12 +16,17 @@ class CustomerSeeder extends Seeder
      */
     public function run()
     {
-        Customer::createCustomer('Hans', 'hayashi0126@ittms.com.tw', '12345');
-        Customer::createCustomer('小姜', 'program03@ittms.com.tw', '12345');
-        Customer::createCustomer('理查', 'richardyuan30@gmail.com', '12345');
-        Customer::createCustomer('阿君', 'ccps961032326@gmail.com', '12345');
-        Customer::createCustomer('之谷', 'pntcwz@gmail.com', '12345');
-        Customer::createCustomer('烏梅', 'hsihung08079@gmail.com', '12345');
-        Customer::createCustomer('yoyo', 'yoyo@writingbeing.com', '12345');
+        $identify = Identity::customer()->value;
+        $this->addIdentify(Customer::createCustomer('Hans', 'hayashi0126@ittms.com.tw', '12345'), $identify);
+        $this->addIdentify(Customer::createCustomer('小姜', 'program03@ittms.com.tw', '12345'), $identify);
+        $this->addIdentify(Customer::createCustomer('理查', 'richardyuan30@gmail.com', '12345'), $identify);
+        $this->addIdentify(Customer::createCustomer('阿君', 'ccps961032326@gmail.com', '12345'), $identify);
+        $this->addIdentify(Customer::createCustomer('之谷', 'pntcwz@gmail.com', '12345'), $identify);
+        $this->addIdentify(Customer::createCustomer('烏梅', 'hsihung08079@gmail.com', '12345'), $identify);
+        $this->addIdentify(Customer::createCustomer('yoyo', 'yoyo@writingbeing.com', '12345'), $identify);
+    }
+
+    private static function addIdentify($customer_id, $identify) {
+        CustomerIdentity::createData($customer_id, $identify);
     }
 }
