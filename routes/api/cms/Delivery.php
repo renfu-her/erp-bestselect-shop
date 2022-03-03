3edf4e3dfab2c1c53c47d9449ddc4a98a9dbac4a
08080 <pntcwz@gmail.com>
2022-03-03 17:57:08 +0800

    新增API 寫入收貨倉對應的入庫商品款式

--- routes/api/cms/Delivery.php
+++ routes/api/cms/Delivery.php
@@ -4,4 +4,5 @@ use App\Http\Controllers\Api\Cms\Commodity\DeliveryCtrl;
 
 Route::group(['prefix' => 'delivery', 'as' => 'delivery.'], function () {
     Route::get('get-select-inbound-list/{product_style_id}', [DeliveryCtrl::class, 'getSelectInboundList'])->name('getSelectInboundList');
+    Route::post('store-receive-depot/{deliveryId}/{itemId}', [DeliveryCtrl::class, 'store']);
 });
