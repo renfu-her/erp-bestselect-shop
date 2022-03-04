317d874e55fb42ce7fa0a5ba61fad4bdba5f82f7
May.Wang <hsihung08079@gmail.com>
2022-03-04 17:33:45 +0800

    綁定消費者 修

--- resources/views/cms/admin/maintenance/bind.blade.php
+++ resources/views/cms/admin/maintenance/bind.blade.php
@@ -16,26 +16,27 @@
             </div>
             <div class="card-body">
                 <x-b-form-group name="name" title="姓名">
-                    <div class="col-form-label">{{ $data->name }}</div>
+                    <div class="form-control">{{ $data->name }}</div>
                 </x-b-form-group>
                 @if (!$customer)
                     <x-b-form-group name="email" title="消費者Email">
                         <div class="input-group mb-3">
                             <input type="email" class="form-control" name="email" placeholder="請輸入email">
-                            <button class="btn btn-outline-secondary" type="button" id="check">查驗</button>
+                            <button class="btn btn-outline-success" type="button" id="check">查驗</button>
                         </div>
                     </x-b-form-group>
                     @error('email')
                         {{ $message }}
                     @enderror
+                    <div class="alert d-none" role="alert"></div>
                     <div class="d-flex justify-content-end mt-3">
                         <a href="{{ route('cms.customer.create', ['bind' => 1]) }}"
-                            class="btn btn-primary px-4">建立消費者並綁定</a>
-                        <button type="submit" class="btn btn-primary px-4">儲存</button>
+                            class="btn btn-primary px-4 -noData d-none">建立消費者並綁定</a>
+                        <button type="submit" class="btn btn-primary px-4 -bind d-none">儲存</button>
                     </div>
                 @else
                     <x-b-form-group name="name" title="已綁定消費者">
-                        <div class="col-form-label">{{ $customer->name }} ({{ $customer->email }})</div>
+                        <div class="form-control">{{ $customer->name }} ({{ $customer->email }})</div>
                     </x-b-form-group>
                 @endif
             </div>
@@ -62,8 +63,7 @@
                         })
                         .catch(err => {
                             console.log(err);
-                        })
-
+                        });
                 }
             })
 
@@ -80,6 +80,24 @@
 
             function processCase(data) {
                 console.log(data);
+                $('div.alert').text(data.message)
+                switch (data.status) {
+                    case '0':
+                        $('div.alert').addClass('alert-success').removeClass('alert-danger d-none');
+                        $('a.-noData').addClass('d-none');
+                        $('button.-bind').removeClass('d-none');
+                        break;
+                    case 'no_data':
+                        $('div.alert').addClass('alert-danger').removeClass('alert-success d-none');
+                        $('a.-noData').removeClass('d-none');
+                        $('button.-bind').addClass('d-none');
+                        break;
+                
+                    default:
+                        $('div.alert').addClass('alert-danger').removeClass('alert-success d-none');
+                        $('a.-noData, button.-bind').addClass('d-none');
+                        break;
+                }
             }
         </script>
     @endpush
