390415fcb8458358b45d47ac85befb1eeee474fa
Yoyo <conan0828+gitlab@gmail.com>
2022-03-09 17:49:27 +0800

    remove unused code

--- database/seeders/IncomeExpenditureSeeder.php
+++ database/seeders/IncomeExpenditureSeeder.php
@@ -7,61 +7,6 @@ use Illuminate\Support\Facades\DB;
 
 class IncomeExpenditureSeeder extends Seeder
 {
-    private const CURRENCY = [
-            [
-                'name' => 'USD-美金',
-                'rate' => 30,
-            ],
-            [
-                'name' => 'JPY-日幣',
-                'rate' => 0.29,
-            ],
-            [
-                'name' => 'EUR-歐元',
-                'rate' => 35,
-            ],
-            [
-                'name' => 'AUD-澳幣',
-                'rate' => 20,
-            ],
-            [
-                'name' => 'CNY-人民幣',
-                'rate' => 4.5,
-            ],
-            [
-                'name' => 'THB-泰幣',
-                'rate' => 1.1,
-            ],
-            [
-                'name' => 'GBP-英鎊',
-                'rate' => 41,
-            ],
-            [
-                'name' => 'KRW-韓幣',
-                'rate' => 0.03,
-            ],
-            [
-                'name' => 'CAD-加拿大幣',
-                'rate' => 20,
-            ],
-            [
-                'name' => 'HKD-港幣',
-                'rate' => 4,
-            ],
-            [
-                'name' => 'NZD-紐西蘭幣',
-                'rate' => 20,
-            ],
-            [
-                'name' => 'SGD-新加坡幣',
-                'rate' => 22,
-            ],
-            [
-                'name' => 'CHF-法郎',
-                'rate' => 32.5,
-            ],
-        ];
-
     /**
      * Run the database seeds.
      *
