e3456aa9acedfb6dc662a5fe9d86960c8f47908e
許志恆 <hayashi0126@gmal.com>
2022-03-09 17:01:05 +0800

    prd collection add done

--- resources/views/cms/commodity/product/settings.blade.php
+++ resources/views/cms/commodity/product/settings.blade.php
@@ -11,18 +11,12 @@
             <div class="row">
                 <div class="col-12 col-sm-6 mb-3">
                     <label class="form-label">自訂群組</label>
-                    <select name="" class="form-select -select2 -multiple" data-placeholder="請選擇自訂群組" multiple hidden>
-                        <option value="1">item 1</option>
-                        <option value="2">item 2</option>
-                        <option value="3">item 3</option>
-                    </select>
-                </div>
-                <div class="col-12 col-sm-6 mb-3">
-                    <label class="form-label">任選折扣群組</label>
-                    <select name="" class="form-select -select2 -multiple" data-placeholder="請選擇任選折扣群組" multiple hidden>
-                        <option value="1">item 1</option>
-                        <option value="2">item 2</option>
-                        <option value="3">item 3</option>
+                    <select name="collection[]" class="form-select -select2 -multiple" data-placeholder="請選擇自訂群組" multiple
+                        hidden>
+                        @foreach ($collections as $collection)
+                            <option value="{{ $collection->id }}" @if (in_array($collection->id, $collection_ids)) selected @endif>
+                                {{ $collection->name }}</option>
+                        @endforeach
                     </select>
                 </div>
             </div>
@@ -31,38 +25,41 @@
             <h6>物流綁定</h6>
             <div>
                 @foreach ($shipments as $key => $value)
-                    @if($value->category === '全家')
+                    @if ($value->category === '全家')
                         <div class="col-12 col-sm-6 mb-3">
-{{--                            <input type="hidden" name="category_id[]" value={{ $value->id }}>--}}
+                            {{-- <input type="hidden" name="category_id[]" value={{ $value->id }}> --}}
                             <label class="form-label">全家(待串接開發)</label>
                             <select name="group_id[]" class="form-select" disabled>
                                 <option value="0">無</option>
                                 @foreach ($value->groupConcat as $key2 => $group)
-                                    <option value="{{ $group->id }}" @if(in_array($group->id,$currentShipment)) selected @endif>{{ $group->name }}</option>
+                                    <option value="{{ $group->id }}" @if (in_array($group->id, $currentShipment)) selected @endif>
+                                        {{ $group->name }}</option>
                                 @endforeach
                             </select>
                         </div>
                     @elseif($value->category === '自取')
-
                     @else
-                    <div class="col-12 col-sm-6 mb-3">
-                        <input type="hidden" name="category_id[]" value={{ $value->id }}>
-                        <label class="form-label">{{ $value->category }}</label>
-                        <select name="group_id[]" class="form-select">
-                            <option value="0">無</option>
-                            @foreach ($value->groupConcat as $key2 => $group)
-                                <option value="{{ $group->id }}" @if(in_array($group->id,$currentShipment)) selected @endif>{{ $group->name }}</option>
-                            @endforeach
-                        </select>
-                    </div>
+                        <div class="col-12 col-sm-6 mb-3">
+                            <input type="hidden" name="category_id[]" value={{ $value->id }}>
+                            <label class="form-label">{{ $value->category }}</label>
+                            <select name="group_id[]" class="form-select">
+                                <option value="0">無</option>
+                                @foreach ($value->groupConcat as $key2 => $group)
+                                    <option value="{{ $group->id }}" @if (in_array($group->id, $currentShipment)) selected @endif>
+                                        {{ $group->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
                     @endif
                 @endforeach
 
                 <div class="col-12 col-sm-6 mb-3">
                     <label class="form-label">自取</label>
-                    <select name="depot_id[]" class="form-select -select2 -multiple" data-placeholder="請選擇門市（可複選）" multiple hidden>
+                    <select name="depot_id[]" class="form-select -select2 -multiple" data-placeholder="請選擇門市（可複選）" multiple
+                        hidden>
                         @foreach ($allPickup as $key => $depot)
-                            <option value="{{ $depot->id }}" @if(in_array($depot->id,$currentPickup)) selected @endif>{{ $depot->name }}</option>
+                            <option value="{{ $depot->id }}" @if (in_array($depot->id, $currentPickup)) selected @endif>
+                                {{ $depot->name }}</option>
                         @endforeach
                     </select>
                 </div>
