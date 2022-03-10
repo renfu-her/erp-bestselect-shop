2a41a19eaff36c7682d99558aed6f5dc33c4ec53
Yoyo <conan0828+gitlab@gmail.com>
2022-03-10 14:20:32 +0800

    create Menu,Route,Permission for Account_Payable and Account_Received

---
+++ app/Http/Controllers/Cms/AccountManagement/AccountReceivedCtrl.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Cms\AccountManagement;
+
+use App\Http\Controllers\Controller;
+use App\Models\AccountReceived;
+use Illuminate\Http\Request;
+
+class AccountReceivedCtrl extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\AccountReceived  $accountReceived
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AccountReceived $accountReceived)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\AccountReceived  $accountReceived
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AccountReceived $accountReceived)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\AccountReceived  $accountReceived
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, AccountReceived $accountReceived)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\AccountReceived  $accountReceived
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AccountReceived $accountReceived)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -52,6 +52,8 @@ Route::group(['prefix' => 'cms', 'as' => 'cms.', 'middleware' => 'auth:user'], f
     require base_path('routes/cms/GeneralLedger.php');
     require base_path('routes/cms/IncomeStatement.php');
     require base_path('routes/cms/IncomeExpenditure.php');
+    require base_path('routes/cms/Ap.php');
+    require base_path('routes/cms/Ar.php');
     require base_path('routes/cms/FirstGrade.php');
     require base_path('routes/cms/Delivery.php');
     require base_path('routes/cms/UserMnt.php');
