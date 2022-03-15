c951ef562e2e07888ce4276e35c860395b0158d4
許志恆 <hayashi0126@gmal.com>
2022-03-15 09:53:02 +0800

    ok

--- database/migrations/2022_03_07_170627_create_discount_table.php
+++ database/migrations/2022_03_07_170627_create_discount_table.php
@@ -17,20 +17,21 @@ class CreateDiscountTable extends Migration
             $table->id();
             $table->string('title')->nullable()->comment('名稱');
             $table->string('sn')->nullable()->comment('序號');
-            $table->string('status_code')->nullable()->comment('狀態碼');
-            $table->string('status_title')->nullable()->comment('狀態名稱');
-            $table->string('category_code')->nullable()->comment('類別code');
-            $table->string('category_title')->nullable()->comment('類別名稱:全館,優惠券');
-            $table->string('discount_type_code')->nullable()->comment('優惠方式');
-            $table->string('discount_type_title')->nullable()->comment('優惠方式標題');
+            $table->string('status_code')->comment('狀態碼');
+            $table->string('status_title')->comment('狀態名稱');
+            $table->string('category_code')->comment('類別code');
+            $table->string('category_title')->comment('類別名稱:全館,優惠券');
+            $table->string('method_code')->comment('優惠方式');
+            $table->string('method_title')->comment('優惠方式標題');
             $table->integer('discount_value')->comment('優惠:金額趴數或是優惠券id');
+            $table->tinyInteger('is_grand_total')->default(1)->comment('是否累計折扣');
+
             $table->integer('usage_count')->default(0)->comment('使用次數');
             $table->integer('max_usage')->nullable()->comment('限制次數');
             $table->integer('min_consume')->default(0)->comment('最低消費金額');
             $table->tinyInteger('is_global')->default(1)->comment('是否全館適用');
             $table->dateTime('start_date')->nullable()->comment('起始時間');
             $table->dateTime('end_date')->nullable()->comment('結束時間');
-
             $table->timestamps();
             $table->softDeletes();
 
