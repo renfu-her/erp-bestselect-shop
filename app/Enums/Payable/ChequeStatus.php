b891069ea7589b5d40b7188d74c6330d4101a1ff
Yoyo <conan0828+gitlab@gmail.com>
2022-03-15 11:54:40 +0800

    add descriptions to Enum PayableStatus and ChequeStatus

--- app/Enums/Payable/ChequeStatus.php
+++ app/Enums/Payable/ChequeStatus.php
@@ -18,4 +18,22 @@ final class ChequeStatus extends Enum
     const OnHold = 3;
     const Returned = 4;
     const Issued = 5;
+
+    public static function getDescription($value): string
+    {
+        switch ($value) {
+            case self::Paid:
+                return '付款';
+            case self::Cashed:
+                return '兌現';
+            case self::OnHold:
+                return '押票';
+            case self::Returned:
+                return '退票';
+            case self::Issued:
+                return '開票';
+            default:
+                return parent::getDescription($value);
+        }
+    }
 }
