64819fb100100bf7fd2c1737312d986b980bd597
許志恆 <hayashi0126@gmal.com>
2022-03-16 12:27:16 +0800

    collection api ok

--- app/Http/Controllers/Api/Cms/Commodity/CollectionCtrl.php
+++ app/Http/Controllers/Api/Cms/Commodity/CollectionCtrl.php
@@ -3,21 +3,19 @@
 namespace App\Http\Controllers\Api\Cms\Commodity;
 
 use App\Http\Controllers\Controller;
+use App\Models\Collection;
 use Illuminate\Http\Request;
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Validator;
 
 class CollectionCtrl extends Controller
 {
-    
+
     public function getCollections(Request $request)
     {
 
         $validator = Validator::make($request->all(), [
-               'supplier_id' => ['required'],
-            
+
         ]);
-        dd( $validator->messages());
 
         if ($validator->fails()) {
             return response()->json([
@@ -26,29 +24,10 @@ class CollectionCtrl extends Controller
             ]);
         }
         $d = $request->all();
-        $options = [];
-        if (isset($d['price'])) {
-            $options['price'] = $d['price'];
-        }
-        if (isset($d['supplier_id'])) {
-            $options['supplier'] = ['condition' => $d['supplier_id']];
-        }
-
-        if (isset($d['consume'])) {
-            $options['consume'] = $d['consume'];
-        }
-
-        
 
         // Arr::get($d, 'supplier_id',''),
 
-        $re = Product::productStyleList(
-            Arr::get($d, 'keyword', ''),
-            Arr::get($d, 'type', ''),
-            [],
-            $options,
-
-        )->paginate(10)->toArray();
+        $re = Collection::select('id','name')->paginate(10)->toArray();
         $re['status'] = '0';
         //   $re['data'] = json_decode(json_encode($re['data']), true);
         return response()->json($re);
--- routes/api/cms/Collection.php
+++ routes/api/cms/Collection.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Api\Cms\Commodity\CollectionCtrl;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'collection', 'as' => 'collection.'], function () {
-    Route::get('get', [CollectionCtrl::class, 'getCollections'])->name('get-collections');
+    Route::post('get', [CollectionCtrl::class, 'getCollections'])->name('get-collections');
   //  Route::post('store-receive-depot', [CollectionCtrl::class, 'store'])->name('create-receive-depot');
   //  Route::get('del-receive-depot/{receiveDepotId}', [CollectionCtrl::class, 'destroy'])->name('del-receive-depot');
 });
