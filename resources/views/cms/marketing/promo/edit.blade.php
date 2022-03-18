b541618ef90d729f3968d6e78e3d0c615d1d2a20
May.Wang <hsihung08079@gmail.com>
2022-03-18 10:00:35 +0800

    新增優惠券，檢查序號

--- resources/views/cms/marketing/promo/edit.blade.php
+++ resources/views/cms/marketing/promo/edit.blade.php
@@ -27,30 +27,20 @@
                         </div>
                         <div class="form-check form-check-inline">
                             <label class="form-check-label">
-                                <input class="form-check-input" name="category" type="radio" value="code">
+                                <input class="form-check-input" name="category" type="radio" value="code" checked>
                                 序號兌換
                             </label>
                         </div>
                     </div>
                 </fieldset>
                 <div class="col-12 mb-3" data-category="code" hidden>
-                    <label class="form-label">優惠劵序號 <span class="text-danger">*</span></label>
-                    <div class="input-group flex-nowrap">
-                        <input type="text" name="" class="form-control" value="" disabled placeholder="可自行輸入或按隨機產生鈕">
-                        <button class="btn btn-success" type="button" id="button-addon2">
+                    <label class="form-label">優惠劵序號 <span class="text-danger">*</span><span class="small text-secondary">（僅接受英數，區分大小寫）</span></label>
+                    <div class="input-group flex-nowrap has-validation">
+                        <input type="text" name="sn" class="form-control" value="" maxlength="20" disabled placeholder="可自行輸入或按隨機產生鈕" autocomplete="off">
+                        <button id="generate_coupon_sn" class="btn btn-success" type="button">
                             <i class="bi bi-shuffle"></i> 隨機產生序號
                         </button>
-                    </div>
-                </div>
-                <div class="col-12 col-sm-6 mb-3">
-                    <label class="form-label">優惠券數量 <span class="text-danger">*</span><span class="small text-secondary">（不限則填0）</span></label>
-                    <input type="number" name="" min="0" value="1000" class="form-control" placeholder="請輸入優惠券數量" required aria-label="優惠券數量">
-                </div>
-                <div class="col-12 col-sm-6 mb-3">
-                    <label class="form-label">最低消費金額 <span class="text-danger">*</span><span class="small text-secondary">（不限則填0）</span></label>
-                    <div class="input-group flex-nowrap">
-                        <span class="input-group-text"><i class="bi bi-currency-dollar"></i></span>
-                        <input type="number" name="" min="0" value="0" class="form-control" placeholder="請輸入優惠券最低消費金額" required>
+                        <div class="valid-feedback invalid-feedback -feedback">序號可使用！</div>
                     </div>
                 </div>
                 <div class="col-12 col-sm-6 mb-3">
@@ -76,6 +66,35 @@
                         <div class="invalid-feedback">
                         </div>
                     </div>
+                </div>
+                <div class="col-12 col-sm-6 mb-3">
+                    <label class="form-label">優惠券數量 <span class="text-danger">*</span><span class="small text-secondary">（不限則填0）</span></label>
+                    <input type="number" name="" min="0" value="1000" class="form-control" placeholder="請輸入優惠券數量" required aria-label="優惠券數量">
+                </div>
+                <div class="col-12 col-sm-6 mb-3">
+                    <label class="form-label">使用優惠券最低消費限制 <span class="text-danger">*</span><span class="small text-secondary">（不限則填0）</span></label>
+                    <div class="input-group flex-nowrap">
+                        <span class="input-group-text"><i class="bi bi-currency-dollar"></i></span>
+                        <input type="number" name="" min="0" value="0" class="form-control" placeholder="請輸入使用優惠券最低消費金額" required>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-6 mb-3">
+                    <label class="form-label">優惠券使用天數<span class="small text-secondary">（未填則表示無限制）</span></label>
+                    <div class="input-group flex-nowrap">
+                        <input type="number" name="" step="1" class="form-control" min="0" value="" placeholder="請輸入優惠券使用天數" norequired>
+                        <span class="input-group-text">天</span>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-6 mb-3">
+                    <label class="form-label">與其他行銷活動併用限制 <span class="text-danger">*</span></label>
+                    <select class="form-select" aria-label="與其他行銷活動併用限制">
+                        <option value="1" selected>無限制</option>
+                        <option value="2">任選折扣</option>
+                        <option value="3">全館折扣</option>
+                        <option value="4">VIP 折扣</option>
+                        <option value="5">推薦碼折扣</option>
+                        <option value="6">加價購</option>
+                    </select>
                 </div>
                 <div class="col-12 mb-3">
                     <label class="form-label">適用商品群組<span class="small text-secondary">（不選為全館適用）</span></label>
@@ -109,7 +128,7 @@
                         <label class="form-label">折扣金額 <span class="text-danger">*</span></label>
                         <div class="input-group flex-nowrap">
                             <span class="input-group-text"><i class="bi bi-currency-dollar"></i></span>
-                            <input type="number" name="" class="form-control" min="0" value="" placeholder="請輸入折扣金額">
+                            <input type="number" name="" class="form-control" min="0" value="" disabled placeholder="請輸入折扣金額">
                         </div>
                     </div>
                     <fieldset class="col-12 col-sm-6 mb-3">
@@ -117,7 +136,7 @@
                         <div class="px-1 pt-1">
                             <div class="form-check form-check-inline">
                                 <label class="form-check-label">
-                                    <input class="form-check-input" name="" type="checkbox" value="1" checked norequired>
+                                    <input class="form-check-input" name="" type="checkbox" value="1" disabled checked norequired>
                                     累計折扣
                                 </label>
                             </div>
@@ -132,44 +151,8 @@
                             <i class="bi bi-info-circle" data-bs-toggle="tooltip" title="例：100 元商品打 8 折為 80 元，請輸入數字 80，等同 80%" data-bs-placement="right"></i>
                         </label>
                         <div class="input-group flex-nowrap">
-                            <input type="number" name="" class="form-control" min="1" max="100" value="" placeholder="請輸入百分比 1 ~ 100">
+                            <input type="number" name="" class="form-control" min="1" max="100" value="" disabled placeholder="請輸入百分比 1 ~ 100">
                             <span class="input-group-text"><i class="bi bi-percent"></i></span>
-                        </div>
-                    </div>
-                </div>
-
-                {{-- 優惠方式：優惠劵 coupon --}}
-                <div class="row mb-3 border rounded mx-0 px-0 pt-2" data-method="coupon" hidden>
-                    <div class="col-12 col-sm-6 mb-3">
-                        <label class="form-label">與其他行銷活動併用限制 <span class="text-danger">*</span></label>
-                        <select class="form-select" aria-label="與其他行銷活動併用限制">
-                            <option value="1" selected>無限制</option>
-                            <option value="2">任選折扣</option>
-                            <option value="3">全館折扣</option>
-                            <option value="4">VIP 折扣</option>
-                            <option value="5">推薦碼折扣</option>
-                            <option value="6">加價購</option>
-                        </select>
-                    </div>
-                    <div class="col-12 col-sm-6 mb-3">
-                        <label class="form-label">優惠券金額 <span class="text-danger">*</span></label>
-                        <div class="input-group flex-nowrap">
-                            <span class="input-group-text"><i class="bi bi-currency-dollar"></i></span>
-                            <input type="number" name="" class="form-control" min="1" value="" placeholder="請輸入優惠券金額">
-                        </div>
-                    </div>
-                    <div class="col-12 col-sm-6 mb-3">
-                        <label class="form-label">優惠券使用天數</label>
-                        <div class="input-group flex-nowrap">
-                            <input type="number" name="" step="1" class="form-control" min="0" value="" placeholder="不填為無限制" norequired>
-                            <span class="input-group-text">天</span>
-                        </div>
-                    </div>
-                    <div class="col-12 col-sm-6 mb-3">
-                        <label class="form-label">使用優惠券最低消費限制 <span class="text-danger">*</span></label>
-                        <div class="input-group flex-nowrap">
-                            <span class="input-group-text"><i class="bi bi-currency-dollar"></i></span>
-                            <input type="number" name="" class="form-control" min="0" value="0">
                         </div>
                     </div>
                 </div>
@@ -179,7 +162,7 @@
         <div id="submitDiv">
             <div class="col-auto">
                 <button type="submit" class="btn btn-primary px-4">儲存</button>
-                <a href="{{ Route('cms.discount.index') }}" class="btn btn-outline-primary px-4"
+                <a href="{{ Route('cms.promo.index') }}" class="btn btn-outline-primary px-4"
                    role="button">返回列表</a>
             </div>
         </div>
@@ -188,12 +171,21 @@
 @once
     @push('sub-scripts')
         <script>
+            const AutoSnLength = 12;
             const AbleControl = { required: true, disabled: false };
             const DisabledControl = { required: false, disabled: true };
 
+            // init
+            setCategory();
+            setMethod();
+
             // 優惠券類型
             $('input[name="category"]').on('change', function () {
-                const category = $(this).val();
+                setCategory();
+            });
+            // 設定優惠券類型
+            function setCategory() {
+                const category = $('input[name="category"]:checked').val();
 
                 if (category === 'code') {
                     $('div[data-category="code"]').prop('hidden', false);
@@ -202,14 +194,15 @@
                     $('div[data-category="code"]').prop('hidden', true);
                     $('div[data-category="code"] input').prop(DisabledControl);
                 }
-            });
-
-            // 優惠劵序號
-
+            }
 
             // 優惠方式
             $('input[name="method_code"]').on('change', function () {
-                const method = $(this).val();
+                setMethod();
+            });
+            // 設定優惠方式
+            function setMethod() {
+                const method = $('input[name="method_code"]:checked').val();
 
                 // hidden
                 $(`div[data-method]:not([data-method="${method}"])`).prop('hidden', true);
@@ -219,7 +212,93 @@
                 $(`div[data-method="${method}"]`).prop('hidden', false);
                 $(`div[data-method="${method}"]`).find('input, select').prop(AbleControl);
                 $(`div[data-method="${method}"]`).find('[norequired]').prop('required', false);
+            }
+
+            $('#form1').submit(function (e) {
+                if ($('input[name="category"]:checked').val() === 'code') {
+                    e.preventDefault();
+                    
+                    const $sn = $('input[name="sn"]');
+                    if ($sn.hasClass('is-valid')) {
+                        $(this).submit();
+                    } if ($sn.hasClass('is-invalid')) {
+                        toast.show('請填入不重複的優惠劵序號', { type: 'danger' });
+                        return false;
+                    } else {
+                        checkSnInput($sn);
+                    }
+                }
+            })
+
+            // 產生優惠劵序號 btn
+            $('#generate_coupon_sn').on('click', function () {
+                generateCouponSn(AutoSnLength);
             });
+            // 檢查序號
+            $('input[name="sn"]').on('change', function () {
+                checkSnInput($(this));
+            });
+            function checkSnInput($snInput) {
+                const sn = $snInput.val();
+                callCheckCouponSnApi(sn).then((res) => {
+                    if (res) {
+                        // 序號可使用
+                        $snInput.removeClass('is-invalid').addClass('is-valid');
+                        $snInput.siblings('-feedback').removeClass('invalid-feedback').addClass('valid-feedback');
+                        $snInput.siblings('-feedback').text('序號可使用！');
+                    } else {
+                        // 序號已使用過
+                        $snInput.removeClass('is-valid').addClass('is-invalid');
+                        $snInput.siblings('-feedback').removeClass('valid-feedback').addClass('invalid-feedback');
+                        $snInput.siblings('-feedback').text('序號已使用過！');
+                    }
+                }).catch((err) => {
+                    console.error(err);
+                    alert('發生錯誤！');
+                });
+            }
+
+            // 產生優惠劵序號
+            function generateCouponSn(len) {
+                let result = '';
+                const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+                for (let index = 0; index < len; index++) {
+                    result += characters.charAt(Math.floor(Math.random() * characters.length));
+                }
+
+                // 檢查重複
+                callCheckCouponSnApi(result).then((res) => {
+                    if (res) {
+                        $('input[name="sn"]').val(result);
+                    } else {
+                        generateCouponSn(AutoSnLength);
+                    }
+                }).catch((err) => {
+                    console.error(err);
+                    alert('發生錯誤！');
+                });
+            }
+
+            // 檢查序號是否重複API
+            function callCheckCouponSnApi(sn) {
+                const _URL = '';
+                let Data = {
+                    sn: sn
+                };
+                console.log('callCheckCouponSnApi', sn);
+                return true;
+
+                return axios.get(_URL, Data).then((result) => {
+                    if (result.status === 200 && re.data.status === '0') {
+                        return result.data;
+                    } else {
+                        return Promise.reject(result);
+                    }
+                }).catch((err) => {
+                    console.error(err);
+                    alert('發生錯誤！');
+                });
+            }
         </script>
     @endpush
 @endonce
