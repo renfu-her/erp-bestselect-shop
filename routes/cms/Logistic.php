d4dd433409ee78d93edec696974c31956212aced
08080 <pntcwz@gmail.com>
2022-03-22 14:09:48 +0800

    ADD MIDDLEWARE

--- routes/cms/Logistic.php
+++ routes/cms/Logistic.php
@@ -6,8 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'logistic','as'=>'logistic.'], function () {
     Route::get('create/{subOrderId}', [LogisticCtrl::class, 'create'])->name('create')->middleware('permission:cms.logistic.create');
     Route::post('store', [LogisticCtrl::class, 'store'])->name('store')->middleware('permission:cms.logistic.create');
-    Route::post('store-consum', [LogisticCtrl::class, 'storeConsum'])->name('storeConsum');
-    Route::post('audit-inbound', [LogisticCtrl::class, 'auditInbound'])->name('auditInbound');
+    Route::post('store-consum', [LogisticCtrl::class, 'storeConsum'])->name('storeConsum')->middleware('permission:cms.logistic.create');
+    Route::post('audit-inbound', [LogisticCtrl::class, 'auditInbound'])->name('auditInbound')->middleware('permission:cms.logistic.create');
     Route::get('delete/{event}/{eventId}/{consumId}', [LogisticCtrl::class, 'destroyItem'])->name('delete')->middleware('permission:cms.logistic.delete');
 
     //修改配送狀態
