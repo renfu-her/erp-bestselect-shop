86160c5b93023a0cabdf6900c9a87fa30e640153
08080 <pntcwz@gmail.com>
2022-03-22 15:41:11 +0800

    新增 刪除物流狀態ROUTE

--- routes/cms/Logistic.php
+++ routes/cms/Logistic.php
@@ -11,6 +11,7 @@ Route::group(['prefix' => 'logistic','as'=>'logistic.'], function () {
     Route::get('delete/{event}/{eventId}/{consumId}', [LogisticCtrl::class, 'destroyItem'])->name('delete')->middleware('permission:cms.logistic.delete');
 
     //修改配送狀態
-    Route::get('change-logistic-status/{event}/{eventId}', [LogisticCtrl::class, 'changeLogisticStatus'])->name('changeLogisticStatus');
-    Route::get('update-logistic-status/{event}/{eventId}/{deliveryId}/{statusCode}', [LogisticCtrl::class, 'updateLogisticStatus'])->name('updateLogisticStatus');
+    Route::get('change-logistic-status/{event}/{eventId}', [LogisticCtrl::class, 'changeLogisticStatus'])->name('changeLogisticStatus')->middleware('permission:cms.logistic.create');
+    Route::get('update-logistic-status/{event}/{eventId}/{deliveryId}/{statusCode}', [LogisticCtrl::class, 'updateLogisticStatus'])->name('updateLogisticStatus')->middleware('permission:cms.logistic.delete');
+    Route::get('delete-logistic-status/{event}/{eventId}/{deliveryId}/{statusCode}', [LogisticCtrl::class, 'deleteLogisticStatus'])->name('deleteLogisticStatus')->middleware('permission:cms.logistic.delete');
 });
