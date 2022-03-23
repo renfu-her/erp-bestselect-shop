50f8b3de633a32c0fda706241ba28604efb7b46f
May.Wang <hsihung08079@gmail.com>
2022-03-23 14:15:32 +0800

    現折優惠編輯

--- resources/views/cms/marketing/discount/edit.blade.php
+++ resources/views/cms/marketing/discount/edit.blade.php
@@ -9,6 +9,12 @@
     <form id="form1" method="post" action="{{ $formAction }}">
         @method('POST')
         @csrf
+        @php
+            $editBlock = $method === 'edit' ? 'disabled' : '';
+        @endphp
+        @if ($method === 'edit')
+            <input type="hidden" name="" value="">
+        @endif
 
         <div class="card shadow p-4 mb-4">
             <div class="row">
@@ -16,7 +22,7 @@
                     <label class="form-label">活動名稱 <span class="text-danger">*</span></label>
                     <input class="form-control @error('title') is-invalid @enderror"
                         value="{{ old('title', $data->title ?? '') }}" name="title" type="text" placeholder="請輸入活動名稱"
-                        required aria-label="活動名稱">
+                        required aria-label="活動名稱" {{ $editBlock }}>
                     @error('title')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
@@ -27,7 +33,7 @@
                         <span class="input-group-text"><i class="bi bi-currency-dollar"></i></span>
                         <input type="number" class="form-control @error('min_consume') is-invalid @enderror"
                             name="min_consume" min="0" value="{{ old('min_consume', $data->min_consume ?? '') }}"
-                            placeholder="請輸入消費金額" required>
+                            placeholder="請輸入消費金額" required {{ $editBlock }}>
                         @error('min_consume')
                             <div class="invalid-feedback">{{ $message }}</div>
                         @enderror
@@ -37,7 +43,7 @@
                     <label class="form-label">活動開始時間 <span class="small text-secondary">（未填則表示現在）</span></label>
                     <div class="input-group has-validation">
                         <input type="datetime-local" name="start_date"
-                            value="{{ old('start_date', $data->start_date ?? '') }}"
+                            value="{{ old('start_date', $data->start_date ?? '') }}" editable
                             class="form-control @error('start_date') is-invalid @enderror" aria-label="活動開始時間" />
                         <button class="btn btn-outline-secondary icon" type="button" data-clear data-bs-toggle="tooltip"
                             title="清空時間"><i class="bi bi-calendar-x"></i>
@@ -53,7 +59,7 @@
                     <label class="form-label">活動結束時間<span class="small text-secondary">（未填則表示不會結束）</span></label>
                     <div class="input-group has-validation">
                         <input type="datetime-local" name="end_date" value="{{ old('end_date', $data->end_date ?? '') }}"
-                            class="form-control @error('end_date') is-invalid @enderror" aria-label="活動結束時間" />
+                            class="form-control @error('end_date') is-invalid @enderror" aria-label="活動結束時間" editable/>
                         <button class="btn btn-outline-secondary icon" type="button" data-clear data-bs-toggle="tooltip"
                             title="清空時間"><i class="bi bi-calendar-x"></i>
                         </button>
@@ -67,7 +73,7 @@
                 <div class="col-12 mb-3">
                     <label class="form-label">適用商品群組<span class="small text-secondary">（不選為全館適用）</span></label>
                     <select name="collection_id[]" multiple class="-select2 -multiple form-select"
-                        data-close-on-select="false" data-placeholder="可多選">
+                        data-close-on-select="false" data-placeholder="可多選" editable>
                         @foreach ($collections as $key => $value)
                             <option value="{{ $value->id }}">{{ $value->name }}</option>
                         @endforeach
@@ -79,7 +85,7 @@
                         @foreach ($dis_methods as $key => $value)
                             <div class="form-check form-check-inline">
                                 <label class="form-check-label">
-                                    <input class="form-check-input" name="method_code" type="radio"
+                                    <input class="form-check-input" name="method_code" type="radio" {{ $editBlock }}
                                         value="{{ $key }}" @if (old('method_code', $data->method_code ?? 'cash') == $key) checked @endif
                                         required>
                                     {{ $value }}
@@ -95,9 +101,8 @@
                         <label class="form-label">折扣金額 <span class="text-danger">*</span></label>
                         <div class="input-group has-validation">
                             <span class="input-group-text"><i class="bi bi-currency-dollar"></i></span>
-                            <input type="number" name="discount_value"
-                                class="form-control @error('discount_value') is-invalid @enderror" placeholder="請輸入折扣金額"
-                                min="0"
+                            <input type="number" name="discount_value" placeholder="請輸入折扣金額" {{ $editBlock }}
+                                class="form-control @error('discount_value') is-invalid @enderror" min="0"
                                 value="{{ old('method_code', $data->method_code ?? '') === 'cash'? old('discount_value', $data->discount_value ?? ''): '' }}">
                             <div class="invalid-feedback">
                                 @if (old('method_code', $data->method_code ?? '') === 'cash')
@@ -113,7 +118,7 @@
                         <div class="px-1 pt-1">
                             <div class="form-check form-check-inline">
                                 <label class="form-check-label">
-                                    <input class="form-check-input" name="is_grand_total" type="checkbox" value="1"
+                                    <input class="form-check-input" name="is_grand_total" type="checkbox" value="1" {{ $editBlock }}
                                         @if (old('is_grand_total', $data->is_grand_total ?? '') == '1') checked @endif norequired>
                                     累計折扣
                                 </label>
@@ -130,7 +135,7 @@
                                 title="例：100 元商品打 8 折為 80 元，請輸入數字 80，等同 80%" data-bs-placement="right"></i>
                         </label>
                         <div class="input-group has-validation">
-                            <input type="number" name="discount_value"
+                            <input type="number" name="discount_value" {{ $editBlock }}
                                 class="form-control @error('discount_value') is-invalid @enderror" min="1" max="100"
                                 value="{{ old('method_code', $data->method_code ?? '') === 'percent'? old('discount_value', $data->discount_value ?? ''): '' }}"
                                 placeholder="請輸入百分比 1 ~ 100">
@@ -150,7 +155,7 @@
                 <div class="row mb-3 border rounded mx-0 px-0 pt-2" data-method="coupon" hidden>
                     <div class="col-12">
                         <label class="form-label">指定贈送優惠券 <span class="text-danger">*</span></label>
-                        <select name="discount_value"
+                        <select name="discount_value" {{ $editBlock }}
                             class="form-select -select2 -single @error('discount_value') is-invalid @enderror"
                             aria-label="指定贈送優惠券">
                             <option value="" selected disabled>請選擇</option>
@@ -166,7 +171,7 @@
                         <p class="fw-light small mark mb-0">
                             <i class="bi bi-exclamation-diamond-fill mx-2 text-warning"></i>
                             此處優惠券為 <a href="{{ Route('cms.promo.create') }}">行銷設定>優惠券/序號</a>
-                            設定之優惠券，若無請先前往新增。（以上未儲存資料將不保留）
+                            設定之優惠券，若無請先前往新增（以上未儲存資料將不保留）。
                         </p>
                     </div>
                 </div>
@@ -192,6 +197,7 @@
                 required: false,
                 disabled: true
             };
+            const editBlock = @json($editBlock);
 
             // init
             setMethod();
@@ -212,6 +218,9 @@
                 $(`div[data-method="${method}"]`).prop('hidden', false);
                 $(`div[data-method="${method}"]`).find('input, select').prop(AbleControl);
                 $(`div[data-method="${method}"]`).find('[norequired]').prop('required', false);
+                if (editBlock) {
+                    $(`div[data-method="${method}"] input:not([editable])`).prop(DisabledControl);
+                }
             }
         </script>
     @endpush
