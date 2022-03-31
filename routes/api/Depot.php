f50ce9ee927f669fcf6f9a8acf89f677378b0df3
08114 <domosn1017@gmail.com>
2022-03-31 17:15:34 +0800

    # add product_depot_select api

---
+++ routes/api/Depot.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\Api\Cms\Commodity\DepotCtrl;
+use Illuminate\Support\Facades\Route;
+
+Route::group(['prefix' => 'depot', 'as' => 'depot.'], function () {
+    Route::get('get-select-product', [DepotCtrl::class, 'get_select_product'])->name('get-select-product');// Route('api.cms.depot.get-select-product')
+});
