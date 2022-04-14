86e2e1fdffab4870cbda36bd03887100a38eff96
08080 <pntcwz@gmail.com>
2022-04-14 18:01:42 +0800

    刪除無用欄位

--- database/migrations/2022_03_24_151155_create_csn_consignment_table.php
+++ database/migrations/2022_03_24_151155_create_csn_consignment_table.php
@@ -20,13 +20,6 @@ class CreateCsnConsignmentTable extends Migration
             $table->string('send_depot_name', 30)->comment('寄件倉庫名稱');
             $table->unsignedBigInteger('receive_depot_id')->comment('收件倉庫ID');
             $table->string('receive_depot_name', 30)->comment('收件倉庫名稱');
-            $table->unsignedBigInteger('ship_temp_id')->nullable()->comment('溫層id');
-            $table->string('ship_temp_name', 10)->nullable()->comment('溫層');
-            $table->unsignedBigInteger('ship_event_id')->nullable()->comment('物流 出貨方式id 對應shi_group.id');
-            $table->string('ship_event', 30)->nullable()->comment('物流名稱');
-            $table->integer('dlv_fee')->default(0)->comment('物流費用');
-            $table->string('logistic_status_code', 10)->nullable()->comment('物流狀態ID');
-            $table->string('logistic_status', 20)->nullable()->comment('物流狀態 檢貨中/理貨中/待配送');
             $table->unsignedBigInteger('create_user_id')->nullable()->comment('建單者ID');
             $table->string('create_user_name', 20)->nullable()->comment('建單者名稱');
             $table->dateTime('scheduled_date')->nullable()->comment('預計入庫日期');
