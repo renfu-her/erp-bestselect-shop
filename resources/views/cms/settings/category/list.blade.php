d0fcfebef4fc7b390cfbcedf4491310ae5cfb7c2
08080 <pntcwz@gmail.com>
2022-04-15 10:54:55 +0800

    修改UI 商品類別	列表	拿掉分頁

--- resources/views/cms/settings/category/list.blade.php
+++ resources/views/cms/settings/category/list.blade.php
@@ -48,12 +48,6 @@
         </table>
     </div>
 </div>
-<div class="row flex-column-reverse flex-sm-row">
-    <div class="col d-flex justify-content-end align-items-center mb-3 mb-sm-0">
-        {{-- 頁碼 --}}
-        <div class="d-flex justify-content-center">{{ $dataList->links() }}</div>
-    </div>
-</div>
 
 
 <!-- Modal -->
