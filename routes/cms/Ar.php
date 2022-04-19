89530bdc6ab8b786f79eba9af0038beb389a1023
08114 <domosn1017@gmail.com>
2022-04-19 17:49:41 +0800

    # adj a/r routes path

--- routes/cms/Ar.php
+++ routes/cms/Ar.php
@@ -4,11 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Cms\AccountManagement\AccountReceivedCtrl;
 
 Route::group(['prefix' => 'ar', 'as' => 'ar.'], function () {
-    Route::get('', [AccountReceivedCtrl::class, 'index'])->name('index')->middleware('permission:cms.ar.index');
-    Route::post('create', [AccountReceivedCtrl::class, 'create'])->name('create')->middleware('permission:cms.ar.create');
-    Route::get('show', [AccountReceivedCtrl::class, 'show'])->name('show')->middleware('permission:cms.ar.show');
+    // Route::get('', [AccountReceivedCtrl::class, 'index'])->name('index')->middleware('permission:cms.ar.index');
+    Route::get('create/{id}', [AccountReceivedCtrl::class, 'create'])->name('create')->middleware('permission:cms.ar.create');
+    // Route::get('show', [AccountReceivedCtrl::class, 'show'])->name('show')->middleware('permission:cms.ar.show');
     Route::post('store', [AccountReceivedCtrl::class, 'store'])->name('store');
-    Route::post('review', [AccountReceivedCtrl::class, 'review'])->name('review')->middleware('permission:cms.ar.review');
+    // Route::post('review', [AccountReceivedCtrl::class, 'review'])->name('review')->middleware('permission:cms.ar.review');
 //    Route::get('edit', [AccountReceivedCtrl::class, 'edit'])->name('edit')->middleware('permission:cms.ar.edit');
 //    Route::post('edit', [AccountReceivedCtrl::class, 'update'])->name('update')->middleware('permission:cms.ar.update');;
 //    Route::get('delete/{id}', [AccountReceivedCtrl::class, 'destroy'])->name('delete')->middleware('permission:cms.ar.delete');
