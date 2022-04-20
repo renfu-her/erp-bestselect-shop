8306f1e82e3542965fb8d2f0ca03bd1ed59cd566
08080 <pntcwz@gmail.com>
2022-04-20 09:40:54 +0800

    修正功能 寄倉單列表篩選審核狀態

--- app/Models/ConsignmentItem.php
+++ app/Models/ConsignmentItem.php
@@ -250,7 +250,7 @@ class ConsignmentItem extends Model
             $result->whereBetween('consignment.created_at_withHIS', [$sDate, $eDate]);
         }
         if (isset($audit_status)) {
-            $result->where('consignment.audit_status', AuditStatus::getDescription($audit_status));
+            $result->where('consignment.audit_status', $audit_status);
         }
         if ($inbound_status) {
             $arr_status = [];
