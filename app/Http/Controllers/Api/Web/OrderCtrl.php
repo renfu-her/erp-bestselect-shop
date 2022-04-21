b591a7b6c3ab9aa2f90119f62d45e24af71c4114
hans <hayashi0126@gmal.com>
2022-04-21 12:18:44 +0800

    order api

--- app/Http/Controllers/Api/Web/OrderCtrl.php
+++ app/Http/Controllers/Api/Web/OrderCtrl.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers\Api\Web;
 
 use App\Enums\Globals\ResponseParam;
+use App\Enums\Order\UserAddrType;
 use App\Enums\Received\ReceivedMethod;
 use App\Http\Controllers\Controller;
+use App\Models\Customer;
 use App\Models\Discount;
+use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class OrderCtrl extends Controller
 {
@@ -41,4 +47,86 @@ class OrderCtrl extends Controller
 
     }
 
+    public function createOrder(Request $request)
+    {
+        $payLoad = json_decode(request()->getContent(), true);
+
+        $validator = Validator::make($payLoad, [
+            "orderer.name" => "required",
+            "orderer.phone" => "required",
+            "orderer.address" => "required",
+            "orderer.city_id" => "required|numeric",
+            "orderer.region_id" => "required|numeric",
+            "orderer.addr" => "required",
+            'orderer.email' => 'required|email',
+            "recipient.name" => "required",
+            "recipient.phone" => "required",
+            "recipient.address" => "required",
+            "recipient.city_id" => "required|numeric",
+            "recipient.region_id" => "required|numeric",
+            "recipient.addr" => "required",
+            "payment" => Rule::in([ReceivedMethod::Cash()->value, ReceivedMethod::CreditCard()->value]),
+            "products" => 'array|required',
+            "products.*.qty" => "required|numeric",
+            "products.*.product_id" => "required",
+            "products.*.product_style_id" => "required",
+            "products.*.shipment_type" => "required",
+            "products.*.shipment_event_id" => "required",
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'E01',
+                'message' => $validator->errors(),
+            ]);
+        }
+
+        DB::beginTransaction();
+
+        $customer = Customer::where('email', $payLoad['orderer']['email'])->get()->first();
+
+        if (!$customer) {
+            $udata = [
+                'name' => $payLoad['orderer']['name'],
+                'email' => $payLoad['orderer']['email'],
+                'password' => '1234',
+            ];
+
+            Customer::createCustomer($udata['name'], $udata['email'], $udata['password']);
+        }
+        $address = [];
+        $address[] = ['name' => $payLoad['orderer']['name'],
+            'phone' => $payLoad['orderer']['phone'],
+            'address' => $payLoad['orderer']['address'],
+            'type' => UserAddrType::orderer()->value];
+
+        $address[] = ['name' => $payLoad['orderer']['name'],
+            'phone' => $payLoad['orderer']['phone'],
+            'address' => $payLoad['orderer']['address'],
+            'type' => UserAddrType::sender()->value];
+
+        $address[] = ['name' => $payLoad['recipient']['name'],
+            'phone' => $payLoad['recipient']['phone'],
+            'address' => $payLoad['recipient']['address'],
+            'type' => UserAddrType::receiver()->value];
+
+        $re = Order::createOrder($payLoad['orderer']['email'], 1, $address, $payLoad['products']);
+
+        if ($re['success'] == '1') {
+            DB::commit();
+            return [
+                'status' => '0',
+                'message' => '',
+                'data' => [
+                    'order_id' => $re['order_id'],
+                ],
+            ];
+        }
+
+        DB::rollBack();
+        return [
+            'stauts' => 'E05',
+            'message' => $re,
+        ];
+    }
 }
