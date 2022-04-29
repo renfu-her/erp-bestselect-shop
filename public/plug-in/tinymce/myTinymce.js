d6ec9808198c189c60e75a27d47b9fc45bec2f57
May.Wang <hsihung08079@gmail.com>
2022-04-29 16:43:42 +0800

    編輯器+原始碼編輯

--- public/plug-in/tinymce/myTinymce.js
+++ public/plug-in/tinymce/myTinymce.js
@@ -1,11 +1,11 @@
 const TINY_OPTION = {
     language: 'zh_TW',
     skin: 'oxide',
-    plugins: `fullscreen lists link image media charmap preview anchor table`,
+    plugins: `code fullscreen lists link image media charmap preview anchor table`,
     menubar: false,  // 'edit insert format table tools'
     toolbar_mode: 'wrap',
     toolbar: 
-        `preview fullscreen | undo redo cut copy paste pastetext | 
+        `code preview fullscreen | undo redo cut copy paste pastetext | 
         alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | 
         styles | fontfamily | fontsize | bold italic underline strikethrough forecolor backcolor codeformat | removeformat |
         link unlink anchor | image media table hr blockquote charmap`
