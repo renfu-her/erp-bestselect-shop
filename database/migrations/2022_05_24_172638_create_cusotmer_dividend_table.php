e3be62e69972363f45a6438ae9c17a9d9f071a57
hans <hayashi0126@gmal.com>
2022-05-25 12:05:25 +0800

    k

--- database/migrations/2022_05_24_172638_create_cusotmer_dividend_table.php
+++ database/migrations/2022_05_24_172638_create_cusotmer_dividend_table.php
@@ -19,10 +19,10 @@ class CreateCusotmerDividendTable extends Migration
             $table->string('category_sn')->comment('來源sn或id');
             $table->integer('customer_id')->comment('會員id');
             $table->integer('weight')->comment('權重'); //有期限 無期限
-            $table->string('flag')->comment('flag');
+            $table->integer('points')->comment('點數'); //有期限 無期限
+            $table->string('flag')->comment('flag'); // 註銷etc
             $table->dateTime('active_sdate')->nullable()->comment('生效時間起');
             $table->dateTime('active_edate')->nullable()->comment('生效時間迄');
-
             $table->timestamps();
         });
     }
