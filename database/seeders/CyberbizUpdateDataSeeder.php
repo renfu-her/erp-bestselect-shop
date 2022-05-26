769756baa0146f5d16b3bc85c358c6ff5c5f7d17
Yoyo <conan0828+gitlab@gmail.com>
2022-05-26 11:16:43 +0800

    comment out unused CyberbizUpdateDataSeeder

--- database/seeders/CyberbizUpdateDataSeeder.php
+++ database/seeders/CyberbizUpdateDataSeeder.php
@@ -7,24 +7,25 @@ use Illuminate\Support\Facades\DB;
 
 class CyberbizUpdateDataSeeder extends Seeder
 {
-    const CYBERBIZ_SKU = 0;
-    const TITLE = 1;
-    const CYBERBIZ_ID = 2;
+//    const CYBERBIZ_SKU = 0;
+//    const TITLE = 1;
+//    const CYBERBIZ_ID = 2;
 
     /**
-     * 匯入缺少的Cyberbiz_id
+     *
      *
      * @return void
      */
     public function run()
     {
-        $jsonFileContents = file_get_contents(database_path('seeders/') . 'imported_data.json');
-        $jsonData = json_decode($jsonFileContents, true);
-
-        foreach ($jsonData['data'] as $productData) {
-            DB::table('prd_products')
-                ->where('sku', '=', $productData[self::CYBERBIZ_SKU])
-                ->update(['cyberbiz_id' => $productData[self::CYBERBIZ_ID]]);
-        }
+        //匯入缺少的Cyberbiz_id
+//        $jsonFileContents = file_get_contents(database_path('seeders/') . 'imported_data.json');
+//        $jsonData = json_decode($jsonFileContents, true);
+//
+//        foreach ($jsonData['data'] as $productData) {
+//            DB::table('prd_products')
+//                ->where('sku', '=', $productData[self::CYBERBIZ_SKU])
+//                ->update(['cyberbiz_id' => $productData[self::CYBERBIZ_ID]]);
+//        }
     }
 }
