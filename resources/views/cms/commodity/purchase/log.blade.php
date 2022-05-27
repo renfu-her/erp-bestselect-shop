fac5bd3efc0fa72f361d6206f77cbfa203b81f7a
08080 <pntcwz@gmail.com>
2022-05-27 16:58:40 +0800

    刪除無用程式碼

--- resources/views/cms/commodity/purchase/log.blade.php
+++ resources/views/cms/commodity/purchase/log.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.main')
 @section('sub-content')
     <h2 class="mb-3">{{$title}} {{ $sn }}</h2>
-    <x-b-pch-navi :id="$id"></x-b-pch-navi>
 
     <div class="card shadow p-4 mb-4">
         <h6>最近變更紀錄</h6>
