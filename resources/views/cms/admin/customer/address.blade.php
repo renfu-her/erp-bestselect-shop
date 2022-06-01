9276b0917de09a6e70af5a4495306f69a471c918
Yoyo <conan0828+gitlab@gmail.com>
2022-06-01 18:06:24 +0800

    add 收件地址管理

---
+++ resources/views/cms/admin/customer/address.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+@section('sub-content')
+    <div>
+        <x-b-customer-navi :customer="$customer"></x-b-customer-navi>
+    </div>
+    <div class="card shadow p-4 mb-4">
+        <h6>預設地址</h6>
+    </div>
+@endsection
