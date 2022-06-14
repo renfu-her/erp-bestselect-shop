e285f55ba307de2ddd853da61ccb174cbb9c4ba5
08080 <pntcwz@gmail.com>
2022-06-14 17:07:59 +0800

    修正

--- resources/views/components/breadcrumb.blade.php
+++ resources/views/components/breadcrumb.blade.php
@@ -2,7 +2,7 @@
     <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
         @php
             //csn_order的ctrl是consignment-order
-            if (isset($value) and isset($value['parent']) and \App\Enums\Delivery\Event::csn_order()->value == $value['parent']) {
+            if (isset($value) and is_array($value) and isset($value['parent']) and \App\Enums\Delivery\Event::csn_order()->value == $value['parent']) {
                 $value['parent'] = 'consignment-order';
             }
         @endphp
