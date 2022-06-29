6c5a3f7df82e1a8080f4ed67def1f9d09b6c9252
08080 <pntcwz@gmail.com>
2022-06-29 10:42:54 +0800

    新增API 取得自取倉、指定倉庫的商品庫存

--- routes/api/Depot.php
+++ routes/api/Depot.php
@@ -6,4 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'depot', 'as' => 'depot.'], function () {
     Route::post('get-select-product', [DepotCtrl::class, 'get_select_product'])->name('get-select-product');// Route('api.cms.depot.get-select-product')
     Route::post('get-select-csn-product', [DepotCtrl::class, 'get_select_csn_product'])->name('get-select-csn-product');
+    Route::post('get-csn-product', [DepotCtrl::class, 'get_csn_product'])->name('get-csn-product');
+    Route::get('get-pickup-depot', [DepotCtrl::class, 'get_pickup_depot'])->name('get-pickup-depot');
 });
