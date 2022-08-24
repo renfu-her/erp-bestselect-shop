9347b4624d6d0120640ec024a2c133f39f2c5f06
Yoyo <conan0828+gitlab@gmail.com>
2022-08-24 14:49:28 +0800

    revise bulletin board permission(UI, controller,route)

--- routes/cms/BulletinBoard.php
+++ routes/cms/BulletinBoard.php
@@ -5,10 +5,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'bulletin_board', 'as' => 'bulletin_board.'], function () {
     Route::get('', [BulletinBoardCtrl::class, 'index'])->name('index');//->middleware('permission:cms.bulletin_board.index');
-    Route::get('create', [BulletinBoardCtrl::class, 'create'])->name('create')->middleware('permission:cms.bulletin_board.create');
+    Route::get('create', [BulletinBoardCtrl::class, 'create'])->name('create');
     Route::post('create', [BulletinBoardCtrl::class, 'store']);
     Route::get('show/{id}', [BulletinBoardCtrl::class, 'show'])->name('show');//->middleware('permission:cms.bulletin_board.show');
-    Route::get('edit/{id}', [BulletinBoardCtrl::class, 'edit'])->name('edit')->middleware('permission:cms.bulletin_board.edit');
+    Route::get('edit/{id}', [BulletinBoardCtrl::class, 'edit'])->name('edit');
     Route::post('edit/{id}', [BulletinBoardCtrl::class, 'update']);
-    Route::get('delete/{id}', [BulletinBoardCtrl::class, 'destroy'])->name('delete')->middleware('permission:cms.bulletin_board.delete');
+    Route::get('delete/{id}', [BulletinBoardCtrl::class, 'destroy'])->name('delete');
 });
