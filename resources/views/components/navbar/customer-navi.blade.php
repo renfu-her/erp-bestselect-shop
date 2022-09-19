7f7a002185d74d13bce135f4d3dc5808f1f5f024
May.Wang <hsihung08079@gmail.com>
2022-09-19 15:26:02 +0800

    顯示mcode

--- resources/views/components/navbar/customer-navi.blade.php
+++ resources/views/components/navbar/customer-navi.blade.php
@@ -1,3 +1,13 @@
+<h2>會員專區 - {{ '烏梅' }}
+    <span class="fs-5 text-secondary">（
+        {{ 'M00000123' }}<button type="button" data-bs-toggle="tooltip" title="複製" 
+        data-mcode="{{ 'M00000123' }}"
+        class="-copy icon icon-btn fs-5 text-primary rounded-circle border-0 p-2">
+            <i class="bi bi-clipboard2-check"></i>
+        </button>）
+    </span>
+</h2>
+
 <ul class="nav pm_navbar" role="tablist">
     <li class="nav-item">
         <a href="{{ Route('cms.customer.order', ['id' => $customer], true) }}"
@@ -30,3 +40,33 @@
     </li>
 </ul>
 <hr class="narbarBottomLine mb-3">
+
+
+@once
+    @push('sub-scripts')
+        <script>
+            $('button.-copy').off('click').on('click', function() {
+                const mcode = $(this).data('mcode');
+                if (navigator && navigator.clipboard) {
+                    navigator.clipboard.writeText(mcode)
+                        .then(() => {
+                            toast.show('已複製頁面連結至剪貼簿', {
+                                type: 'success'
+                            });
+                        }).catch((err) => {
+                            console.error('剪貼簿錯誤', err);
+                            toast.show('請手動複製連結：<br>' + mcode, {
+                                title: '發生錯誤',
+                                type: 'danger'
+                            });
+                        });
+                } else {
+                    toast.show('請手動複製連結：<br>' + mcode, {
+                        title: '不支援剪貼簿功能',
+                        type: 'danger'
+                    });
+                }
+            });
+        </script>
+    @endpush
+@endonce
