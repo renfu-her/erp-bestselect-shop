621c636e899779c78497f93c640b8dcd74bbde5e
08080 <pntcwz@gmail.com>
2022-09-22 11:04:03 +0800

    修正 $hasCreatedFinalPayment 欄位會被disable無法將值傳送到後端 所以再加同數值的隱藏欄位

--- resources/views/cms/commodity/purchase/edit.blade.php
+++ resources/views/cms/commodity/purchase/edit.blade.php
@@ -218,8 +218,11 @@
                                     <input type="hidden" name="name[]" value="{{ old('name.'. $psItemKey, $psItemVal->title?? '') }}">
                                     <input type="hidden" name="sku[]" value="{{ old('sku.'. $psItemKey, $psItemVal->sku?? '') }}">
                                     <input type="hidden" name="estimated_cost[]" value="{{ old('estimated_cost.'. $psItemKey, $psItemVal->estimated_cost?? '') }}">
-                                    <input type="hidden" name="num[]" value="{{ old('num.'. $psItemKey, $psItemVal->num?? '') }}">
-                                    <input type="hidden" name="price[]" value="{{ old('price.'. $psItemKey, $psItemVal->price?? '') }}">
+
+                                    @if(true == $hasCreatedFinalPayment)
+                                        <input type="hidden" name="num[]" value="{{ old('num.'. $psItemKey, $psItemVal->num?? '') }}">
+                                        <input type="hidden" name="price[]" value="{{ old('price.'. $psItemKey, $psItemVal->price?? '') }}">
+                                    @endif
                                 </th>
                                 <td data-td="name">{{ old('name.'. $psItemKey, $psItemVal->title?? '') }}</td>
                                 <td data-td="sku">{{ old('sku.'. $psItemKey, $psItemVal->sku?? '') }}</td>
