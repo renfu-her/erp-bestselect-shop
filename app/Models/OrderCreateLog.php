7f2c9168bc3200c073844a60a316ce3e7748aa32
May.Wang <hsihung08079@gmail.com>
2022-09-22 14:49:12 +0800

    mod table name

--- app/Models/OrderCreateLog.php
+++ app/Models/OrderCreateLog.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class OrderCreateLog extends Model
 {
     use HasFactory;
-    protected $table = 'ord_order_create_log';
+    protected $table = 'ord_create_order_log';
     protected $guarded = [];
 }
