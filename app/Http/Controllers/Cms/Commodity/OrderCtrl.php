1430ff2a4e6f8e7d95536ce2456b69f0539cbaff
08114 <domosn1017@gmail.com>
2022-09-22 18:11:48 +0800

    # adj logistic_po

--- app/Http/Controllers/Cms/Commodity/OrderCtrl.php
+++ app/Http/Controllers/Cms/Commodity/OrderCtrl.php
@@ -1506,9 +1506,9 @@ class OrderCtrl extends Controller
         $logistics_grade_name = AllGrade::find($paying_order->logistics_grade_id)->eachGrade->code . ' ' . AllGrade::find($paying_order->logistics_grade_id)->eachGrade->name;
 
         if ($sub_order->projlgt_order_sn) {
-            $logistics_grade_name = $logistics_grade_name . ' #' . $sub_order->projlgt_order_sn;
+            $logistics_grade_name = $logistics_grade_name . ' ' . $sub_order->ship_group_name . ' #' . $sub_order->projlgt_order_sn;
         } else {
-            $logistics_grade_name = $logistics_grade_name . ' #' . $sub_order->package_sn;
+            $logistics_grade_name = $logistics_grade_name . ' ' . $sub_order->ship_group_name . ' #' . $sub_order->package_sn;
         }
 
         $payable_data = PayingOrder::get_payable_detail($paying_order->id);
