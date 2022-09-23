23808c2e7833f14ad977a34b4b77980e19310ec7
08080 <pntcwz@gmail.com>
2022-09-23 11:55:21 +0800

    修正

--- app/Http/Controllers/Api/Web/OrderCtrl.php
+++ app/Http/Controllers/Api/Web/OrderCtrl.php
@@ -445,7 +445,7 @@ class OrderCtrl extends Controller
 
         $payinfo = null;
         $payinfo['category'] = $payLoad['category'] ?? null;
-        $payinfo['invoice_method'] = $d['invoice_method'] ?? InvoiceMethod::e_inv()->key();
+        $payinfo['invoice_method'] = $d['invoice_method'] ?? InvoiceMethod::e_inv()->key;
         $payinfo['inv_title'] = $payLoad['inv_title'] ?? null;
         $payinfo['buyer_ubn'] = $payLoad['buyer_ubn'] ?? null;
         $payinfo['love_code'] = $payLoad['love_code'] ?? null;
