a9e280f4424d67b3fd0d7f76a537aa48691a5736
hans <hayashi0126@gmal.com>
2022-09-26 16:27:03 +0800

    ok

--- app/Models/Sn.php
+++ app/Models/Sn.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
 
 class Sn extends Model
 {
@@ -13,15 +15,18 @@ class Sn extends Model
 
     public static function createSn($type, $prefix)
     {
+        DB::beginTransaction();
         $sn = $prefix . date("Ymd") . str_pad((self::whereDate('created_at', '=', date('Y-m-d'))->where('type', $type)
-                ->get()
+                ->lockForUpdate()->get()
                 ->count()) + 1, 4, '0', STR_PAD_LEFT);
+
 
         self::create([
             'sn' => $sn,
             'type' => $type,
         ]);
 
+        DB::commit();
         return $sn;
     }
 
