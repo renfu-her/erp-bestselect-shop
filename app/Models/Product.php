fb81ccb374ffc5c459fdc26c934f942523544b64
Yoyo <conan0828+gitlab@gmail.com>
2022-09-27 18:17:27 +0800

    productList新增過濾酒類商品

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -107,10 +107,24 @@ class Product extends Model
 
             $re->leftJoin('collection_prd as cprd', 'product.id', '=', 'cprd.product_id_fk')
                 ->leftJoin('collection as colc', 'colc.id', '=', 'cprd.collection_id_fk')
-                ->addSelect(['colc.name as collection_name', 'collection_id_fk'])
-                ->where('cprd.collection_id_fk', '=', $options['collection']);
+                ->addSelect(['colc.name as collection_name', 'collection_id_fk']);
 
+            //使用在酒類商品搜尋
+            if (!isset($options['is_liquor'])) {
+                $re->where('cprd.collection_id_fk', '=', $options['collection']);
+            }
         }
+
+        if (isset($options['collection']) &&
+            isset($options['is_liquor'])) {
+            if ($options['is_liquor'] == 1) {
+                $re->where('colc.is_liquor', '=', 1)
+                    ->where('colc.is_public', '=', 1);
+            } elseif ($options['is_liquor'] == 0) {
+                $re->where('colc.is_liquor', '=', 0);
+            }
+        }
+
         if (isset($options['category_id']) && $options['category_id']) {
             $re->where('product.category_id', '=', $options['category_id']);
         }
