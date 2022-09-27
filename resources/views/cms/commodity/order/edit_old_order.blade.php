eaf44a820db29ba6ac2bafde10cbda9abbe2fc26
hans <hayashi0126@gmal.com>
2022-09-27 11:21:50 +0800

    銷貨備註bug處理

--- resources/views/cms/commodity/order/edit_old_order.blade.php
+++ resources/views/cms/commodity/order/edit_old_order.blade.php
@@ -72,7 +72,8 @@
                                     <tr>
                                         <td>物流</td>
                                         <td>
-                                            <select name="ship_category[]" class="form-select form-select-sm -sx w-100" required>
+                                            <select name="ship_category[]" class="form-select form-select-sm -sx w-100"
+                                                required>
                                                 @foreach ($shipmentCategory as $key => $value)
                                                     <option value="{{ $value->code }}"
                                                         @if ($subOrder->ship_category == $value->code) selected @endif>
@@ -81,7 +82,8 @@
                                             </select>
                                         </td>
                                         <td>
-                                            <select name="ship_event_id[]" class="-select2 -single form-select form-select-sm -sx">
+                                            <select name="ship_event_id[]"
+                                                class="-select2 -single form-select form-select-sm -sx">
                                                 @foreach ($shipEvent[$subOrder->ship_category] as $key => $value)
                                                     <option value="{{ $value->id }}"
                                                         @if ($subOrder->ship_event_id == $value->id) selected @endif>
@@ -95,20 +97,21 @@
                                             <div class="input-group input-group-sm flex-nowrap">
                                                 <span class="input-group-text">$</span>
                                                 <input class="form-control -sx" name="dlv_fee[]" type="number"
-                                                aria-label="運費" value="{{ $subOrder->dlv_fee }}" required>
+                                                    aria-label="運費" value="{{ $subOrder->dlv_fee }}" required>
                                             </div>
                                         </td>
                                     </tr>
-                                    <tr>
-                                        <td>銷貨備註</td>
-                                        <td colspan="4"> <input class="form-control form-control-sm -sx"
-                                                name="sub_order_note[]" aria-label="運費" value="{{ $subOrder->note }}"></td>
-                                    </tr>
+
                                 </tbody>
                             </table>
                         </div>
                     </div>
                 @endif
+                <div class="card-header px-4  text-secondary border-top border-bottom-0">銷貨備註</div>
+                <div class="card-body px-4 py-2">
+                    <input class="form-control form-control-sm -sx" name="sub_order_note[]" aria-label="運費"
+                        value="{{ $subOrder->note }}">
+                </div>
             </div>
         @endforeach
 
