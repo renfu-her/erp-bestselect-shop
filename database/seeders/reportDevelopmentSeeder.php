9e1810e7c7a37336d86929be214af2a40258e77d
hans <hayashi0126@gmal.com>
2022-10-04 16:16:04 +0800

    報表處理

---
+++ database/seeders/reportDevelopmentSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\RptUserReportMonthly;
+use App\Models\RptOrganizeReportMonthly;
+use Illuminate\Database\Seeder;
+
+class reportDevelopmentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+       //  RptUserReportMonthly::report();
+       RptOrganizeReportMonthly::report();
+    }
+}
