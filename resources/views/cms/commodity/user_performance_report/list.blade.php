bd1d52a360f5ef1a3efe841af912e5d438eddc47
May.Wang <hsihung08079@gmail.com>
2022-10-05 17:02:16 +0800

    業績報表

--- resources/views/cms/commodity/user_performance_report/list.blade.php
+++ resources/views/cms/commodity/user_performance_report/list.blade.php
@@ -23,43 +23,42 @@
                                 <div class="form-check form-check-inline">
                                     <label class="form-check-label">
                                         <input class="form-check-input" name="type" type="radio"
-                                            value="{{ $key }}" @if ($key == $cond['type']) checked @endif>
+                                            value="{{ $key }}" @if ($key === $cond['type']) checked @endif>
                                         {{ $value }}
                                     </label>
                                 </div>
                             @endforeach
                         </div>
                     </fieldset>
-                    <div class="col-12 col-sm-6 mb-3">
+                    <div class="col-12 col-sm-6 mb-3 -year">
                         <label class="form-label">年度</label>
                         <select class="form-select -select" name="year" aria-label="年度">
                             @foreach ($year as $value)
-                                <option value="{{ $value }}" @if ($value == $cond['year']) selected @endif>
+                                <option value="{{ $value }}" @if ($value === $cond['year']) selected @endif>
                                     {{ $value }}</option>
                             @endforeach
                         </select>
                     </div>
-                    <div class="col-12 col-sm-6 mb-3">
-                        <label class="form-label">月份</label>
-                        <select class="form-select -select" name="month" aria-label="月份">
-                            @for ($i = 1; $i < 13; $i++)
-                                <option value="{{ $i }}" @if ($i == $cond['month']) selected @endif>
-                                    {{ $i }}月</option>
-                            @endfor
-                        </select>
-                    </div>
-                    <div class="col-12 col-sm-6 mb-3">
+                    <div class="col-12 col-sm-6 mb-3 -season" @if ($cond['type'] !== 'season') hidden @endif >
                         <label class="form-label">季</label>
                         <select class="form-select -select" name="season" aria-label="季">
                             @foreach ($season as $key => $value)
-                                <option value="{{ $key }}" @if ($key == $cond['season']) selected @endif>
+                                <option value="{{ $key }}" @if ($key === $cond['season']) selected @endif>
                                     第{{ $value }}季</option>
                             @endforeach
                         </select>
                     </div>
+                    <div class="col-12 col-sm-6 mb-3 -month"@if ($cond['type'] !== 'month') hidden @endif >
+                        <label class="form-label">月份</label>
+                        <select class="form-select -select" name="month" aria-label="月份">
+                            @for ($i = 1; $i < 13; $i++)
+                                <option value="{{ $i }}" @if ($i === $cond['month']) selected @endif>
+                                    {{ $i }}月</option>
+                            @endfor
+                        </select>
+                    </div>
                 </div>
                 <div class="col">
-
                     <button type="submit" class="btn btn-primary px-4">搜尋</button>
                 </div>
             </div>
@@ -73,7 +72,7 @@
 
         <div class="table-responsive tableOverBox">
             <table class="table table-striped tableList">
-                <thead class="small align-middle">
+                <thead class="small">
                     <tr>
                         <th scope="col" style="width:40px">#</th>
                         <th scope="col">部門名稱</th>
@@ -124,17 +123,18 @@
                             <td class="text-center">{{ $data->total_price }}</td>
                         </tr>
                     @endforeach
-                    <tr>
-                        <td></td>
-                        <td>合計</td>
-                        <td class="text-center">{{ $on_price }}</td>
-                        <td class="text-center">{{ $on_gross_profit }}</td>
-                        <td class="text-center">{{ $off_price }}</td>
-                        <td class="text-center">{{ $off_gross_profit }}</td>
-                        <td class="text-center">{{ $total_gross_profit }}</td>
-                        <td class="text-center">{{ $total_price }}</td>
-                    </tr>
                 </tbody>
+                <tfoot>
+                    <tr>
+                        <th colspan="2">合計</th>
+                        <th class="text-center">{{ $on_price }}</th>
+                        <th class="text-center">{{ $on_gross_profit }}</th>
+                        <th class="text-center">{{ $off_price }}</th>
+                        <th class="text-center">{{ $off_gross_profit }}</th>
+                        <th class="text-center">{{ $total_gross_profit }}</th>
+                        <th class="text-center">{{ $total_price }}</th>
+                    </tr>
+                </tfoot>
             </table>
         </div>
     </div>
@@ -142,18 +142,31 @@
 @once
     @push('sub-styles')
         <style>
-            .icon.-close_eye+span.label::before {
-                content: '不';
+            h4 {
+                color: #415583;
             }
         </style>
     @endpush
     @push('sub-scripts')
         <script>
-            // 阿眉～
-            let selectedUser = @json($cond['department']);
-            $('#dataPerPageElem').on('change', function(e) {
-                $('input[name=data_per_page]').val($(this).val());
-                $('#search').submit();
+            $('input[name="type"][type="radio"]').on('change', function(e) {
+                const val = $(this).val();
+                switch (val) {
+                    case 'year':    // 年度
+                        $('div.-season, div.-month').prop('hidden', true);
+                        break;
+                    case 'season':    // 季
+                        $('div.-month').prop('hidden', true);
+                        $('div.-season').prop('hidden', false);
+                        break;
+                    case 'month':    // 月份
+                        $('div.-season').prop('hidden', true);
+                        $('div.-month').prop('hidden', false);
+                        break;
+                
+                    default:
+                        break;
+                }
             });
         </script>
     @endpush
