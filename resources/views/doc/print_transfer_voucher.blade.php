067c92e055faff44ca158b1e1a76dfa24825d55a
May.Wang <hsihung08079@gmail.com>
2022-10-06 12:29:01 +0800

    轉帳傳票 列印

--- resources/views/doc/print_transfer_voucher.blade.php
+++ resources/views/doc/print_transfer_voucher.blade.php
@@ -11,6 +11,10 @@
             font-family: "Nunito", "Noto Sans TC", sans-serif;
             position: relative;
         }
+        @page {
+            margin: 5mm 0 0;
+            /* 邊界 */
+        }
 
         .font {
             display: inline-block;
@@ -41,55 +45,42 @@
 <body style="margin-top: 0px;">
     <div style="position: absolute; left: 0; top: 0; width:100%">
         <div style="text-align: center;">
-            <div style="font-size: x-large; font-family:標楷體">{{ $applied_company->company }}</div>
-            <div style="font-size: small; margin: 1px auto;">
-                <span>地址：{{ $applied_company->address }}</span>
-                <span style="margin-left: 1.5rem;">電話：{{ $applied_company->phone }}</span>
-                <span style="margin-left: 1.5rem;">傳真：{{ $applied_company->fax }}</span>
-            </div>
+            <div style="font-size: x-large; font-family:標楷體">{{ $voucher->company_name }}</div>
+            <div style="font-size: x-large; font-family:標楷體">轉帳傳票</div>
+            <div style="font-size: x-large; font-family:標楷體">中華民國 {{ date('Y', strtotime($voucher->tv_voucher_date)) - 1911 }} 年 {{ date('m', strtotime($voucher->tv_voucher_date)) }} 月 {{ date('d', strtotime($voucher->tv_voucher_date)) }} 日</div>
 
-            <div style="font-size: x-large; font-family:標楷體">
-                轉　帳　傳　票
-            </div>
-            <hr width="710" style="margin: .5rem auto;">
             <div>
                 <table width="710" style="font-size:small;text-align:left;border:0;margin: 0 auto;">
                     <tbody>
                         <tr>
-                            <td width="50%">付款單號：{{ $stitute_order->so_sn }}</td>
-                            <td width="50%">製表日期：{{ date('Y-m-d', strtotime($stitute_order->so_created_at)) }}</td>
-                        </tr>
-                        <tr>
-                            <td>單據編號：</td>
-                            <td>付款日期：{{ $stitute_order->so_payment_date ? date('Y-m-d', strtotime($stitute_order->so_payment_date)) : '' }}</td>
-                        </tr>
-                        <tr>
-                            <td>支付對象：{{ $stitute_order->so_client_name }}</td>
-                            <td>承辦人：{{ $stitute_order->creator_name }}</td>
+                            <td width="50%">傳票編號：{{ $day_emd_item ? $day_emd_item->sn : '' }}</td>
+                            <td width="50%" style="text-align: right;">單號：{{ $voucher->tv_sn }}</td>
                         </tr>
                     </tbody>
                 </table>
+
                 <hr width="710" style="margin: .5rem auto;">
-                <table width="710" cellpadding="5"
-                    style="font-size:small;margin:0 auto;border-collapse:collapse;">
+                <table width="710" cellpadding="5" style="font-size:small;margin:0 auto;border-collapse:collapse;">
                     <thead style="text-align: left;">
                         <tr height="24">
-                            <th scope="col" width="40%" style="padding-bottom:7px;">代墊項目</th>
-                            <th scope="col" width="8%" style="padding-bottom:7px;text-align: right;">數量</th>
-                            <th scope="col" width="8%" style="padding-bottom:7px;text-align: right;">單價</th>
-                            <th scope="col" width="10%" style="padding-bottom:7px;text-align: right;">應付金額</th>
-                            <th scope="col" width="34%" style="padding-bottom:7px;">備註</th>
+                            <th scope="col" width="32%" style="padding-bottom:7px;">會計科目</th>
+                            <th scope="col" width="32%" style="padding-bottom:7px;">摘要</th>
+                            <th scope="col" width="8%" style="padding-bottom:7px;">幣別</th>
+                            <th scope="col" width="8%" style="padding-bottom:7px;text-align: right;">匯率</th>
+                            <th scope="col" width="10%" style="padding-bottom:7px;text-align: right;">借方</th>
+                            <th scope="col" width="10%" style="padding-bottom:7px;text-align: right;">貸方</th>
                         </tr>
                     </thead>
                     <tbody style="text-align: left;">
-                        @if($stitute_order->so_items)
-                        @foreach(json_decode($stitute_order->so_items) AS $data)
+                        @if($voucher->tv_items)
+                        @foreach(json_decode($voucher->tv_items) as $value)
                         <tr>
-                            <td>{{ $data->grade_code . ' ' . $data->grade_name . ' ' . $data->summary }}</td>
-                            <td style="text-align: right;">{{ $data->qty }}</td>
-                            <td style="text-align: right;">{{ number_format($data->price, 2) }}</td>
-                            <td style="text-align: right;">{{ number_format($data->total_price) }}</td>
-                            <td>@php echo $data->memo ?? '' @endphp</td>
+                            <td>{{ $value->grade_code . ' ' . $value->grade_name }}</td>
+                            <td>{{ $value->summary }}</td>
+                            <td>{{ $value->currency_name }}</td>
+                            <td style="text-align: right;">{{ $value->rate }}</td>
+                            <td style="text-align: right;">{{ $value->debit_credit_code == 'debit' ? number_format($value->final_price, 2) : '' }}</td>
+                            <td style="text-align: right;">{{ $value->debit_credit_code == 'credit' ? number_format($value->final_price, 2) : '' }}</td>
                         </tr>
                         @endforeach
                         @endif
@@ -100,24 +91,23 @@
                     style="font-size:small;margin:0 auto;border-collapse:collapse;">
                     <thead>
                         <tr height="24">
-                            <td width="20%">合　　計：</td>
-                            <td width="36%" style="text-align: right;">（{{ $zh_price }}）</td>
-                            <td width="10%" style="text-align: right;">{{ number_format($stitute_order->so_price) }}</td>
-                            <td width="34%"></td>
+                            <td width="32%">合計</td>
+                            <td width="32%" style="text-align: right;">{{ $voucher->tv_debit_price != $voucher->tv_credit_price ? '（借貸不平）' : '' }}</td>
+                            <td width="8%"></td>
+                            <td width="8%"></td>
+                            <td width="10%" style="text-align: right;">{{ number_format($voucher->tv_debit_price) }}</td>
+                            <td width="10%" style="text-align: right;">{{ number_format($voucher->tv_credit_price) }}</td>
                         </tr>
                     </thead>
-                    <tbody style="text-align: left;">
-
-                    </tbody>
+                    <tbody style="text-align: left;"></tbody>
                 </table>
                 <hr width="710" style="margin: .5rem auto;">
                 <table width="710" style="font-size:small;text-align:left;border:0;margin: 0 auto;">
                     <tbody>
                         <tr>
-                            <td width="25%">財務主管：</td>
-                            <td width="25%">會計：{{ $stitute_order->accountant_name }}</td>
-                            <td width="25%">商品主管：</td>
-                            <td width="25%">商品負責人：</td>
+                            <td width="25%">主管：</td>
+                            <td width="25%">會計：{{ $voucher->accountant_name }}</td>
+                            <td width="25%">承辦人：{{ $voucher->creator_name }}</td>
                         </tr>
                   </tbody>
                 </table>
