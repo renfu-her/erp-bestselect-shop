9ad246eeb78524dfb3b8cba7de1ce80480ff0765
08080 <pntcwz@gmail.com>
2022-10-11 17:28:51 +0800

    隱藏按鈕 因此行為未做其他入庫單之類的判斷 若已入庫又執行 則資料會錯亂

--- resources/views/cms/commodity/delivery/edit.blade.php
+++ resources/views/cms/commodity/delivery/edit.blade.php
@@ -110,9 +110,9 @@
         <div id="submitDiv">
             <div class="col-auto">
                 <button type="submit" class="btn btn-primary px-4" @if (isset($delivery->audit_date)) disabled @endif>送出</button>
-                @if (isset($delivery->audit_date))
-                    <a href="{{ Route('cms.delivery.store_cancle', ['deliveryId' => $delivery->id ]) }}" class="btn btn-outline-dark px-4" role="button">取消送出審核</a>
-                @endif
+{{--                @if (isset($delivery->audit_date))--}}
+{{--                    <a href="{{ Route('cms.delivery.store_cancle', ['deliveryId' => $delivery->id ]) }}" class="btn btn-outline-dark px-4" role="button">取消送出審核</a>--}}
+{{--                @endif--}}
                 @if($delivery->event == App\Enums\Delivery\Event::order()->value)
                     <a href="{{ Route('cms.order.detail', ['id' => $order_id, 'subOrderId' => $eventId ]) }}" class="btn btn-outline-primary px-4" role="button">返回明細</a>
                 @elseif($delivery->event == App\Enums\Delivery\Event::consignment()->value)
