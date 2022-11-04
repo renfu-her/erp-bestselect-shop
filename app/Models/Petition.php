df358d7d3e34e10e0dacd33c5ff8617cc18ba865
hans <hayashi0126@gmal.com>
2022-11-04 11:10:42 +0800

    wait audit model done

--- app/Models/Petition.php
+++ app/Models/Petition.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Petition extends Model
 {
@@ -11,4 +12,91 @@ class Petition extends Model
     protected $table = 'pet_petition';
     protected $guarded = [];
 
+    public static function dataList()
+    {
+
+        $concatString = concatStr([
+            'user_id' => 'audit.user_id',
+            'user_name' => 'user.name',
+            'user_title' => 'user.title',
+            'checked_at' => 'IFNULL(audit.checked_at,"")',
+        ]);
+
+        $sub = DB::table('pet_audit as audit')
+            ->leftJoin('usr_users as user', 'user.id', '=', 'audit.user_id')
+            ->select('audit.source_id')
+            ->selectRaw('(' . $concatString . ') as users')
+            ->orderBy('audit.step')
+            ->groupBy('audit.source_id')
+            ->where('audit.source_type', 'petition');
+
+        $re = DB::table('pet_petition as petition')
+            ->leftJoinSub($sub, 'audit', 'audit.source_id', '=', 'petition.id')
+            ->leftJoin('usr_users as user', 'petition.user_id', '=', 'user.id')
+            ->select(['petition.*', 'audit.*', 'user.name as user_name']);
+
+        return $re;
+
+    }
+
+    public static function waitAuditlist($user_id)
+    {
+
+        $sub = DB::table('pet_audit as audit')
+            ->whereNull('audit.checked_at')
+            ->select('audit.source_id')
+            ->selectRaw('max(audit.step) as step')
+            ->groupBy('audit.source_id')
+            ->where('audit.source_type', 'petition');
+
+        $sub2 = DB::table('pet_audit as audit')
+            ->select('audit.*')
+            ->joinSub($sub, 'sub_audit', function ($join) {
+                $join->on('sub_audit.source_id', '=', 'audit.source_id')
+                    ->on('sub_audit.step', '=', 'audit.step');
+            })
+            ->where('audit.user_id', $user_id);
+
+        return $sub2;
+        /*
+        dd($sub2->get());
+      
+        $re = DB::table('pet_petition as pet')
+            ->select('pet.*')
+            ->joinSub($sub2, 'audit2', 'audit2.source_id', '=', 'pet.id');
+
+        dd($re->get());
+        */
+    }
+
+    public static function createPetition($user_id, $title, $content, $orders = [])
+    {
+        DB::beginTransaction();
+
+        $sn = 'PET' . str_pad((self::lockForUpdate()->get()
+                ->count()) + 1, 9, '0', STR_PAD_LEFT);
+
+        $id = self::create([
+            'user_id' => $user_id,
+            'title' => $title,
+            'content' => $content,
+            'sn' => $sn,
+        ])->id;
+
+        $org = UserOrganize::auditList($user_id);
+
+        //  dd($org);
+
+        DB::table('pet_audit')->insert(array_map(function ($n, $idx) use ($id) {
+            return [
+                'step' => $idx + 1,
+                'user_id' => $n->user_id,
+                'source_id' => $id,
+                'source_type' => 'petition',
+            ];
+        }, $org, array_keys($org)));
+
+        DB::commit();
+
+    }
 }
--- resources/views/cms/admin_management/petition/list.blade.php
+++ resources/views/cms/admin_management/petition/list.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 @section('sub-content')
     <h2 class="mb-4">申議書</h2>
-  
+
     <div class="card shadow p-4 mb-4">
         <div class="row mb-4">
             <div class="col">
@@ -13,7 +13,8 @@
                 顯示
                 <select class="form-select d-inline-block w-auto" id="dataPerPageElem" aria-label="表格顯示筆數">
                     @foreach (config('global.dataPerPage') as $value)
-                        <option value="{{ $value }}" @if ($data_per_page == $value) selected @endif>{{ $value }}</option>
+                        <option value="{{ $value }}" @if ($data_per_page == $value) selected @endif>
+                            {{ $value }}</option>
                     @endforeach
                 </select>
                 筆
@@ -23,54 +24,65 @@
         <div class="table-responsive tableOverBox">
             <table class="table table-striped tableList">
                 <thead>
-                <tr>
-                    <th scope="col" style="width:10%">#</th>
-                    <th scope="col">主旨</th>
-                    <th scope="col">重要性</th>
-                    <th scope="col">公告日期</th>
-                    <th scope="col">公告者</th>
-                    <th scope="col" class="text-center">編輯</th>
-                    <th scope="col" class="text-center">刪除</th>
-                </tr>
+                    <tr>
+                        <th scope="col" style="width:10%">#</th>
+                        <th scope="col" class="text-center">編輯</th>
+                        <th scope="col">申請人</th>
+                        <th scope="col">主旨</th>
+                        <th scope="col">內容</th>
+                        <th scope="col">簽核狀態</th>
+                        <th scope="col">新增日期</th>
+
+
+                        <th scope="col" class="text-center">刪除</th>
+                    </tr>
                 </thead>
                 <tbody>
-                @foreach ($dataList ?? [] as $key => $data)
-                    <tr>
-                        <th scope="row">{{ $key + 1 }}</th>
-                        <td>
-                            <a href="{{ Route('cms.petition.show', ['id' => $data->id], true) }}">
-                                {{ $data->title ?? '' }}
-                            </a>
-                        </td>
-                        <td @class(['fw-bold text-danger' => $data->weight === '3',
-                                            'text-success' => $data->weight === '2'])>
-                            {{ \App\Enums\AdminManagement\Weight::getDescription($data->weight) }}
-                        </td>
-                        <td>
-                            {{ date('Y/m/d', strtotime($data->created_at ?? '')) }}
-                        </td>
-                        <td>{{ $data->user_name }}</td>
+                    @foreach ($dataList ?? [] as $key => $data)
+                        <tr>
+                            <th scope="row">{{ $key + 1 }}</th>
                             <td class="text-center">
-                                @if(\Illuminate\Support\Facades\Auth::user()->id === $data->user_id)
+                                @if (\Illuminate\Support\Facades\Auth::user()->id === $data->user_id)
                                     <a href="{{ Route('cms.petition.edit', ['id' => $data->id], true) }}"
-                                       data-bs-toggle="tooltip" title="編輯"
-                                       class="icon icon-btn fs-5 text-primary rounded-circle border-0">
+                                        data-bs-toggle="tooltip" title="編輯"
+                                        class="icon icon-btn fs-5 text-primary rounded-circle border-0">
                                         <i class="bi bi-pencil-square"></i>
                                     </a>
                                 @endif
                             </td>
-                            <td class="text-center">
-                                @if(\Illuminate\Support\Facades\Auth::user()->id === $data->user_id)
-                                    <a href="javascript:void(0)"
-                                       data-href="{{ Route('cms.petition.delete', ['id' => $data->id], true) }}"
-                                       data-bs-toggle="modal" data-bs-target="#confirm-delete"
-                                       class="icon -del icon-btn fs-5 text-danger rounded-circle border-0">
-                                        <i class="bi bi-trash"></i>
-                                    </a>
-                                @endif
+                            <td>
+                                {{ $data->user_name }}
                             </td>
-                    </tr>
-                @endforeach
+                            <td>
+                                <a href="{{ Route('cms.petition.edit', ['id' => $data->id], true) }}">
+                                    {{ $data->title ?? '' }}
+                                </a>
+                            </td>
+                            <td>{{ $data->content }}</td>
+                            <td>
+                                @foreach ($data->users as $key => $user)
+                                    <div> {{ $user->user_title }} {{ $user->user_name }}
+                                        <span>{{ $user->checked_at }}</span>
+                                    </div>
+                                @endforeach
+
+                            </td>
+                            <td>
+                                {{ date('Y/m/d', strtotime($data->created_at ?? '')) }}
+                            </td>
+
+
+                            <td class="text-center">
+                                <a href="javascript:void(0)"
+                                    data-href="{{ Route('cms.petition.delete', ['id' => $data->id], true) }}"
+                                    data-bs-toggle="modal" data-bs-target="#confirm-delete"
+                                    class="icon -del icon-btn fs-5 text-danger rounded-circle border-0">
+                                    <i class="bi bi-trash"></i>
+                                </a>
+                            </td>
+
+                        </tr>
+                    @endforeach
                 </tbody>
             </table>
         </div>
@@ -78,7 +90,7 @@
     <div class="row flex-column-reverse flex-sm-row">
         <div class="col d-flex justify-content-end align-items-center mb-3 mb-sm-0">
             {{-- 頁碼 --}}
-            {{--        <div class="d-flex justify-content-center">{{ $dataList->links() }}</div>--}}
+            <div class="d-flex justify-content-center">{{ $dataList->links() }}</div>
         </div>
     </div>
 
@@ -91,7 +103,6 @@
             <a class="btn btn-danger btn-ok" href="#">確認並刪除</a>
         </x-slot>
     </x-b-modal>
-
 @endsection
 
 @once
