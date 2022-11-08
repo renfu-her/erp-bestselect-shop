9dd501a8a8c59af8ebb9190db9e91164c552a9ac
hans <hayashi0126@gmal.com>
2022-11-08 11:40:09 +0800

    bug and midify

---
+++ app/Models/Audit.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class Audit extends Model
+{
+    use HasFactory;
+    protected $table = 'pet_audit';
+    protected $guarded = [];
+    public $timestamps = false;
+
+    public static function waitAuditlist($user_id, $type)
+    {
+
+        $sub = DB::table('pet_audit as audit')
+            ->whereNull('audit.checked_at')
+            ->select('audit.source_id')
+            ->selectRaw('max(audit.step) as step')
+            ->groupBy('audit.source_id')
+            ->where('audit.source_type', $type);
+
+        $sub2 = DB::table('pet_audit as audit')
+            ->select('audit.*')
+            ->joinSub($sub, 'sub_audit', function ($join) {
+                $join->on('sub_audit.source_id', '=', 'audit.source_id')
+                    ->on('sub_audit.step', '=', 'audit.step');
+            })
+            ->where('audit.user_id', $user_id);
+
+        return $sub2;
+    }
+
+    public static function addAudit($user_id, $source_id, $type)
+    {
+        $org = UserOrganize::auditList($user_id);
+
+        //  dd($org);
+
+        self::insert(array_map(function ($n, $idx) use ($source_id, $type) {
+            return [
+                'step' => $idx + 1,
+                'user_id' => $n->user_id,
+                'source_id' => $source_id,
+                'source_type' => $type,
+            ];
+        }, $org, array_keys($org)));
+    }
+
+    public static function confirm($pid, $user_id, $type)
+    {
+
+        self::where('source_id', $pid)
+            ->where('source_type', $type)
+            ->where('user_id', $user_id)->update(['checked_at' => now()]);
+    }
+}
