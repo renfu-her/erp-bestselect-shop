89d04be3bb1a914fdf247c94527db83dc28523d3
hans <hayashi0126@gmal.com>
2023-02-03 16:53:44 +0800

    ok

--- resources/views/cms/settings/b2e/edit.blade.php
+++ resources/views/cms/settings/b2e/edit.blade.php
@@ -1,52 +1,59 @@
 @extends('layouts.main')
 @section('sub-content')
+    <h2 class="mb-4">
+        @if ($method === 'create')
+            新增
+        @else
+            編輯
+        @endif 企業網
+    </h2>
 
-<h2 class="mb-4">@if ($method === 'create') 新增 @else 編輯 @endif 企業網</h2>
-
-    <form method="post" action="">
+    <form method="post" action="" enctype="multipart/form-data">
         @method('POST')
         @csrf
 
         <div class="card shadow p-4 mb-4">
             <div class="row">
                 <x-b-form-group name="title" title="企業名稱" required="true" class="col-12 col-sm-6">
-                    <input class="form-control @error('title') is-invalid @enderror" name="title" 
-                        value="" />
+                    <input class="form-control @error('title') is-invalid @enderror" name="title"
+                        value="{{ old('title', $data->title ?? '') }}" required />
                 </x-b-form-group>
                 <x-b-form-group name="short_title" title="企業簡稱" required="true" class="col-12 col-sm-6">
-                    <input class="form-control @error('short_title') is-invalid @enderror" name="short_title" 
-                        value="" />
+                    <input class="form-control @error('short_title') is-invalid @enderror" name="short_title"
+                        value="{{ old('short_title', $data->short_title ?? '') }}" required />
                 </x-b-form-group>
                 <x-b-form-group name="vat_no" title="統一編號" required="true" class="col-12 col-sm-6">
-                    <input class="form-control @error('vat_no') is-invalid @enderror" name="vat_no" 
-                        value="" />
+                    <input class="form-control @error('vat_no') is-invalid @enderror" name="vat_no"
+                        value="{{ old('vat_no', $data->vat_no ?? '') }}" required />
                 </x-b-form-group>
                 <x-b-form-group name="tel" title="企業電話" required="false" class="col-12 col-sm-6">
-                    <input class="form-control @error('tel') is-invalid @enderror" name="tel" 
-                        value="" />
+                    <input class="form-control @error('tel') is-invalid @enderror" name="tel"
+                        value="{{ old('tel', $data->tel ?? '') }}" />
                 </x-b-form-group>
                 <x-b-form-group name="ext" title="分機號碼" required="false" class="col-12 col-sm-6">
-                    <input class="form-control @error('ext') is-invalid @enderror" name="ext" 
-                        value="" />
+                    <input class="form-control @error('ext') is-invalid @enderror" name="ext"
+                        value="{{ old('ext', $data->ext ?? '') }}" />
                 </x-b-form-group>
                 <x-b-form-group name="contact_person" title="窗口" required="true" class="col-12 col-sm-6">
-                    <input class="form-control @error('contact_person') is-invalid @enderror" name="contact_person" 
-                        value="" />
+                    <input class="form-control @error('contact_person') is-invalid @enderror" name="contact_person"
+                        value="{{ old('contact_person', $data->contact_person ?? '') }}" required />
                 </x-b-form-group>
                 <x-b-form-group name="contact_tel" title="窗口手機" required="false" class="col-12 col-sm-6">
-                    <input class="form-control @error('contact_tel') is-invalid @enderror" name="contact_tel" 
-                        value="" />
+                    <input class="form-control @error('contact_tel') is-invalid @enderror" name="contact_tel"
+                        value="{{ old('contact_tel', $data->contact_tel ?? '') }}" />
                 </x-b-form-group>
                 <x-b-form-group name="contact_email" title="窗口信箱" required="false" class="col-12 col-sm-6">
-                    <input class="form-control @error('contact_email') is-invalid @enderror" name="contact_email" 
-                        value="" />
+                    <input class="form-control @error('contact_email') is-invalid @enderror" name="contact_email"
+                        value="{{ old('contact_email', $data->contact_email ?? '') }}" />
                 </x-b-form-group>
                 <x-b-form-group name="email" title="合約起訖日" required="true" class="col-12">
                     <div class="input-group has-validation">
                         <input type="date" class="form-control -startDate @error('contract_sdate') is-invalid @enderror"
-                            name="contract_sdate" value="" aria-label="合約起始" />
+                            name="contract_sdate" value="{{ old('contract_sdate', $data->contract_sdate ?? '') }}"
+                            aria-label="合約起始" required />
                         <input type="date" class="form-control -endDate @error('contract_edate') is-invalid @enderror"
-                            name="contract_edate" value="" aria-label="合約結束" />
+                            name="contract_edate" value="{{ old('contract_edate', $data->contract_edate ?? '') }}"
+                            aria-label="合約結束" required />
                         <div class="invalid-feedback">
                             @error('contract_sdate')
                                 {{ $message }}
@@ -58,29 +65,34 @@
                     </div>
                 </x-b-form-group>
                 <x-b-form-group name="salechannel_id" title="銷售通路" required="true" class="col-12 col-sm-6">
-                    <select class="form-select">
-                        {{-- @foreach ($ as $) --}}
-                            <option value="">喜鴻購物企業網</option>
-                        {{-- @endforeach --}}
+                    <select class="form-select" name="salechannel_id">
+                        @foreach ($salechannels as $value)
+                            <option value="{{ $value->id }}" @if ($value->id == old('salechannel_id', $data->salechannel_id ?? '')) selected @endif>
+                                {{ $value->title }}</option>
+                        @endforeach
                     </select>
                 </x-b-form-group>
                 <x-b-form-group name="user_id" title="業務員" required="false" class="col-12 col-sm-6">
-                    <select class="form-select">
-                        {{-- @foreach ($ as $) --}}
-                            <option value="" >無</option>
-                        {{-- @endforeach --}}
+                    <select class="form-select" name="user_id">
+                        <option value="">無</option>
+                        @foreach ($users as $value)
+                            <option value="{{ $value->id }}" @if ($value->id == old('user_id', $data->user_id ?? '')) selected @endif>
+                                {{ $value->name }}</option>
+                        @endforeach
                     </select>
                 </x-b-form-group>
                 <x-b-form-group name="email" title="LOGO" required="false" class="col-12">
-                    <input class="form-control @error('file') is-invalid @enderror" type="file" name="img" accept=".jpg,.jpeg,.png,.gif">
+                    <input class="form-control @error('file') is-invalid @enderror" type="file" name="img"
+                        accept=".jpg,.jpeg,.png,.gif">
                 </x-b-form-group>
-            </div>
+                <img style="max-width:100px" src="@if (true == isset($data->img)) {{ asset($data->img) }} @endif" />
 
+            </div>
             @if ($method === 'edit')
                 <input type='hidden' name='id' value="" />
             @endif
             @error('id')
-            <div class="alert alert-danger mt-3">{{ $message }}</div>
+                <div class="alert alert-danger mt-3">{{ $message }}</div>
             @enderror
         </div>
 
@@ -94,14 +106,13 @@
 @endsection
 @once
     @push('sub-styles')
-    <style>
-        label.form-check-label[data-type]::after{
-            content: attr(data-type)
-        }
-    </style>
+        <style>
+            label.form-check-label[data-type]::after {
+                content: attr(data-type)
+            }
+        </style>
     @endpush
     @push('sub-scripts')
-        <script>
-        </script>
+        <script></script>
     @endpush
 @endonce
