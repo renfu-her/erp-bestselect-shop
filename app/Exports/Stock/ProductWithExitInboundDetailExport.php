603134954285c43c89d55ddb908f41cdd078ff62
08080 <pntcwz@gmail.com>
2023-02-16 17:03:41 +0800

    修改 庫存管理 匯出庫存明細EXCEL 20230216 line 理查:幫我多匯出以下欄位 公開否/線上是否開啟/線下是否開啟/ 類別/可售數量

--- app/Exports/Stock/ProductWithExitInboundDetailExport.php
+++ app/Exports/Stock/ProductWithExitInboundDetailExport.php
@@ -2,6 +2,7 @@
 
 namespace App\Exports\Stock;
 
+use App\Models\Category;
 use App\Models\ProductStyle;
 use App\Models\PurchaseInbound;
 use Illuminate\Support\Facades\DB;
@@ -38,8 +39,13 @@ class ProductWithExitInboundDetailExport implements FromQuery, WithHeadings
             , '倉庫'
             , '理貨倉庫存'
             , '寄倉庫存'
-            , '官網可售數量'
+            , '官網可售數量(超賣)'
             , '廠商名稱'
+            , '公開否'
+            , '線上是否開啟'
+            , '線下是否開啟'
+            , '類別'
+            , '可售數量'
         ];
     }
 
@@ -49,6 +55,7 @@ class ProductWithExitInboundDetailExport implements FromQuery, WithHeadings
             ->leftJoinSub(ProductStyle::getChannelSubList(), 'channelSub', function($join) {
                 $join->on('channelSub.style_id', 'tb_tslwei.id');
             })
+            ->leftJoin(app(Category::class)->getTable() . ' as category', 'category.id', '=', 'tb_tslwei.category_id')
             ->select('tb_tslwei.type_title'
                 ,'tb_tslwei.product_title'
                 , 'tb_tslwei.spec'
@@ -60,8 +67,13 @@ class ProductWithExitInboundDetailExport implements FromQuery, WithHeadings
                 , 'tb_tslwei.depot_name'
                 , 'tb_tslwei.total_in_stock_num'
                 , 'tb_tslwei.total_in_stock_num_csn'
-                , 'tb_tslwei.in_stock'
+                , DB::raw('CONCAT(ifnull(tb_tslwei.in_stock, 0), "(", ifnull(tb_tslwei.overbought, 0), ")") as in_stock_overbought')
                 , 'tb_tslwei.suppliers_name'
+                , DB::raw('IF(tb_tslwei.public = 1, "是", "否") as public')
+                , DB::raw('IF(tb_tslwei.online = 1, "是", "否") as online')
+                , DB::raw('IF(tb_tslwei.offline = 1, "是", "否") as offline')
+                , 'category.category'
+                , 'tb_tslwei.in_stock_original'
             )
             ->orderBy('tb_tslwei.product_id')
             ->orderBy('tb_tslwei.id')
