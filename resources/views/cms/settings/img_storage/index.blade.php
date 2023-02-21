fd9eb708041024213104427e91573e5b5cefc9e2
May.Wang <hsihung08079@gmail.com>
2023-02-21 11:01:52 +0800

    新增 img view

---
+++ resources/views/cms/settings/img_storage/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>上傳圖片</title>
+</head>
+<body>
+    <div id="content">
+        <div>
+            <h2>圖片上傳</h2>
+        </div>
+
+        <div>
+            <h2>圖片檢視</h2>
+        </div>
+    </div>
+</body>
+</html>
