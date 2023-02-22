fd967de3681340c0d08f0febfab571a7bd01a800
08114 <domosn1017@gmail.com>
2023-02-22 11:09:19 +0800

    # adj view render

--- resources/views/cms/marketing/promo/edit.blade.php
+++ resources/views/cms/marketing/promo/edit.blade.php
@@ -211,6 +211,32 @@
 
                     <div class="col-12 mb-3">
                         <label class="form-label">內容 <span class="text-danger">*</span></label>
+                        <div class="alert alert-primary" role="alert">
+                            <i class="bi bi-info-circle-fill"></i> 參數使用說明：（點選框中文字可自動複製）
+                            <div class="d-flex flex-wrap" style="list-style-type: none;">
+                                <div class="col-12 col-md-6">
+                                    <li><code class="border border-secondary">{$name}</code> - 消費者姓名</li>
+                                </div>
+                                <div class="col-12 col-md-6">
+                                    <li><code class="border border-secondary">{$email}</code> - 消費者Email</li>
+                                </div>
+                                <div class="col-12 col-md-6">
+                                    <li><code class="border border-secondary">{$sn}</code> - 發送優專券訂單編號</li>
+                                </div>
+                                <div class="col-12 col-md-6">
+                                    <li><code class="border border-secondary">{$title}</code> - 優惠券活動名稱</li>
+                                </div>
+                                <div class="col-12 col-md-6">
+                                    <li><code class="border border-secondary">{$c_title}</code> - 優惠類型</li>
+                                </div>
+                                <div class="col-12 col-md-6">
+                                    <li><code class="border border-secondary">{$d_value}</code> - 優惠內容</li>
+                                </div>
+                                <div class="col-12 col-lg-6">
+                                    <li><code class="border border-secondary">{$active_edate}</code> - 優惠券到期日</li>
+                                </div>
+                            </div>
+                        </div>
                         <textarea id="editor" name="mail_content" hidden></textarea>
                     </div>
                 </div>
@@ -416,6 +442,22 @@
             }).then((editors) => {
                 editors[0].setContent(mail_content);
             });
+
+            $('.alert-primary code').on('click', function () {
+                let range, selection;
+                if (document.body.createTextRange) {
+                    range = document.body.createTextRange();
+                    range.moveToElementText(this);
+                    range.select();
+                } else if (window.getSelection) {
+                    selection = window.getSelection();
+                    range = document.createRange();
+                    range.selectNodeContents(this);
+                    selection.removeAllRanges();
+                    selection.addRange(range);
+                }
+                copyToClipboard(range.toString());
+            })
         </script>
     @endpush
 @endonce
