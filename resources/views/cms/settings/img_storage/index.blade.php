77d5ef83fd7cf44100f4c7c0fa74a4d0d8ea44bc
May.Wang <hsihung08079@gmail.com>
2023-02-22 15:10:28 +0800

    mod bug, UI

--- resources/views/cms/settings/img_storage/index.blade.php
+++ resources/views/cms/settings/img_storage/index.blade.php
@@ -63,19 +63,19 @@
                         <table style="width:700px; max-width:100%;margin:auto;" border="1" cellspacing="0">
                             <tr>
                                 <td style="" class="title">上傳說明</td>
-                                <td style="padding: 2px;">檔案格式：JPG / JPEG / PNG / GIF</td>
+                                <td style="padding: 2px;">檔案格式：JPG / JPEG / PNG / BMP</td>
                             </tr>
                             <tr>
                                 <td style="" class="title">選擇檔案</td>
                                 <td>
-                                    <input type="file" name="file" accept=".jpg,.jpeg,.png,.gif" style="width:100%;">
+                                    <input type="file" name="file" accept=".jpg,.jpeg,.png,.bmp" style="width:100%;">
                                 </td>
                             </tr>
                         </table>
-                        @error('file')
-                            {{ $message }}
-                        @enderror
                     </div>
+                    @error('file')
+                        <div style="color: red;text-align: center;">{{ $message }}</div>
+                    @enderror
                     <div style="margin-top: 10px;text-align: center;">
                         <button type="submit" style="padding:1px 15px;">上傳</button>
                     </div>
@@ -101,16 +101,16 @@
                         </table>
                     </div>
                     <div style="margin-top: 10px;text-align: center;">
-                        {{-- @if (按查詢後) --}}
-                            <span style="margin-right: 20px;">
-                                總筆數：{{ $dataList->count() }}（共 {{ $dataList->lastPage() }}  頁）
-                                <select name="page">
-                                    @for($i=0;$i< $dataList->lastPage();$i++)
-                                    <option value="{{ $i+1 }}">第 {{ $i+1 }} 頁</option>
-                                    @endfor
-                                </select>
-                            </span>
-                        {{-- @endif --}}
+                        <span style="margin-right: 20px;">
+                            總筆數：{{ $dataList->total() }}（共 {{ $dataList->lastPage() }}  頁）
+                            <select name="page">
+                                @for($i=0; $i< $dataList->lastPage(); $i++)
+                                    <option value="{{ $i+1 }}" @if($i+1 === $dataList->currentPage()) selected @endif>
+                                        第 {{ $i+1 }} 頁
+                                    </option>
+                                @endfor
+                            </select>
+                        </span>
                         <button type="submit" style="padding:1px 15px;">查詢</button>
                     </div>
                 </form>
@@ -124,8 +124,6 @@
             @foreach($dataList as $key => $value)
                 @php
                     $url = getImageUrl($value->url,true);
-
-                    
                 @endphp
                 <div class="item">
                     <table>
@@ -142,21 +140,13 @@
                             </td>
                         </tr>
                         <tr>
-                            <td colspan="2">圖庫 (點圖預覽)</td>
+                            <td colspan="2">點圖預覽</td>
                         </tr>
                         <tr>
                             <td colspan="2">
                                 <a href="{{  $url }}" target="_blank">
                                     <img src="{{  $url }}" alt="">
                                 </a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td colspan="2">CDN同步</td>
-                        </tr>
-                        <tr>
-                            <td colspan="2">
-                                <img src="https://img.bestselection.com.tw/2022PG01/20220914114543293.jpg" alt="">
                             </td>
                         </tr>
                     </table>
