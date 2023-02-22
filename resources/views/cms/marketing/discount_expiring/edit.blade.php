1174a1963da77bbf82bb1c4d1539a45939b5b163
May.Wang <hsihung08079@gmail.com>
2023-02-22 11:03:22 +0800

    mod UI

--- resources/views/cms/marketing/discount_expiring/edit.blade.php
+++ resources/views/cms/marketing/discount_expiring/edit.blade.php
@@ -19,26 +19,26 @@
                 <div class="alert alert-primary" role="alert">
                     <i class="bi bi-info-circle-fill"></i> 參數使用說明：（點選框中文字可自動複製）
                     <div class="d-flex flex-wrap" style="list-style-type: none;">
-                        <div class="col-12 col-sm-6">
-                            <li><code class="border border-secondary">{$active_edate}</code> - 優惠券到期日</li>
-                        </div>
-                        <div class="col-12 col-sm-6">
+                        <div class="col-12 col-md-6">
                             <li><code class="border border-secondary">{$name}</code> - 消費者姓名</li>
                         </div>
-                        <div class="col-12 col-sm-6">
+                        <div class="col-12 col-md-6">
                             <li><code class="border border-secondary">{$email}</code> - 消費者Email</li>
                         </div>
-                        <div class="col-12 col-sm-6">
+                        <div class="col-12 col-md-6">
                             <li><code class="border border-secondary">{$sn}</code> - 發送優專券訂單編號</li>
                         </div>
-                        <div class="col-12 col-sm-6">
+                        <div class="col-12 col-md-6">
                             <li><code class="border border-secondary">{$title}</code> - 優惠券活動名稱</li>
                         </div>
-                        <div class="col-12 col-sm-6">
+                        <div class="col-12 col-md-6">
                             <li><code class="border border-secondary">{$c_title}</code> - 優惠類型</li>
                         </div>
-                        <div class="col-12 col-sm-6">
+                        <div class="col-12 col-md-6">
                             <li><code class="border border-secondary">{$d_value}</code> - 優惠內容</li>
+                        </div>
+                        <div class="col-12 col-lg-6">
+                            <li><code class="border border-secondary">{$active_edate}</code> - 優惠券到期日</li>
                         </div>
                     </div>
                 </div>
