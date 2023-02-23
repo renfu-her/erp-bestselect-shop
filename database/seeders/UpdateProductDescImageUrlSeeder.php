b93d42e20945feaf400d4d94222cffd1aecfbdc5
Yoyo <conan0828+gitlab@gmail.com>
2023-02-23 09:22:51 +0800

    修改CDN圖檔的seeder

--- database/seeders/UpdateProductDescImageUrlSeeder.php
+++ database/seeders/UpdateProductDescImageUrlSeeder.php
@@ -11,7 +11,7 @@ use simplehtmldom\HtmlDocument;
 class UpdateProductDescImageUrlSeeder extends Seeder
 {
     /**
-     * 把產品描述的圖片連結從images-besttour.cdn.hinet.net改成edgio-cdn-pix.besttour.com.tw
+     * 把產品描述的圖片連結從edgio-cdn-pix.besttour.com.tw改成images-besttour.cdn.hinet.net
      * @return void
      */
     public function run()
@@ -28,9 +28,9 @@ class UpdateProductDescImageUrlSeeder extends Seeder
             if ($productDatum->desc){
                 print_r('id:' . ($prdKey + 1));
                 $descHTML = (new HtmlDocument())->load($productDatum->desc);
-                $descImages = $descHTML->find('img[src^="https://images-besttour.cdn.hinet.net/"]');
+                $descImages = $descHTML->find('img[src^="https://edgio-cdn-pix.besttour.com.tw/"]');
                 foreach ($descImages as $key => $descImage) {
-                    preg_match('~^https://images-besttour.cdn.hinet.net/(.*)~', $descImage->src, $matchSubUrl);
+                    preg_match('~^https://edgio-cdn-pix.besttour.com.tw/(.*)~', $descImage->src, $matchSubUrl);
                     $descImage->src = ImageDomain::CDN . $matchSubUrl[1];
                 }
                 Product::where('id', $productDatum->id)
