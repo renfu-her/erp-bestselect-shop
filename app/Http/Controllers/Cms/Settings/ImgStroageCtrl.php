a21e7faa314230395efce6d0cea990694f6def4f
hans <hayashi0126@gmal.com>
2023-03-02 15:19:56 +0800

    img

--- app/Http/Controllers/Cms/Settings/ImgStroageCtrl.php
+++ app/Http/Controllers/Cms/Settings/ImgStroageCtrl.php
@@ -132,7 +132,7 @@ class ImgStroageCtrl extends Controller
                 ->resize(1000, 1000, function ($constraint) {
                     $constraint->aspectRatio();
                     $constraint->upsize();
-                })->encode('webp', 90);
+                })->encode('webp', 50);
         } catch (\Exception $e) {
             dd($e);
             return false;
