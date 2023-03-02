5706c16ab496e62acd4fe72740d2aae98d4f1d00
hans <hayashi0126@gmal.com>
2023-03-02 16:52:10 +0800

    ok

--- app/Models/userMenu.php
+++ app/Models/userMenu.php
@@ -193,6 +193,8 @@ return
                 "title" => "企業網管理",
                 "controller_name" => "B2eCompanyCtrl",
                 "route_name" => "cms.b2e-company.index",
+            ],
+            [  
                 "title" => "團控查詢帳號",
                 "controller_name" => "ErpTravelCtrl",
                 "route_name" => "cms.erp-travel.index",
