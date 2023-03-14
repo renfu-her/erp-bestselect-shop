18ecd0cdea706167a02af4f0aef89f6d7c6be614
May.Wang <hsihung08079@gmail.com>
2023-03-14 11:37:12 +0800

    版型三

--- resources/views/cms/frontend/homepage/template/edit.blade.php
+++ resources/views/cms/frontend/homepage/template/edit.blade.php
@@ -40,6 +40,9 @@
                         @enderror
                     </div>
                 </fieldset>
+                @php
+                    $old_type = old('style_type', $data->style_type ?? '');
+                @endphp
                 <fieldset class="col-12 mb-3">
                     <legend class="col-form-label p-0 mb-2">選擇版型 <span class="text-danger">*</span></legend>
                     <div class="row">
@@ -47,7 +50,7 @@
                             <div class="col-12 col-sm-6 col-xl-4 col-xxl-3 mb-3">
                                 <label class="d-flex flex-wrap -template">
                                     <input type="radio" name="style_type" value="{{ $val }}"
-                                        class="form-check-input" required @if ($val == old('style_type', $data->style_type ?? '')) checked @endif>
+                                        class="form-check-input" required @if ($val == $old_type) checked @endif>
                                     {{ $description = App\Enums\Homepage\TemplateStyleType::getDescription($val) }}
                                     <div class="mb-1 p-0 col-12">
                                         <div class="me-2 -preview">
@@ -63,26 +66,24 @@
                         @endforeach
                     </div>
                 </fieldset>
-                <div class="col-12 col-sm-6 mb-3">
+                <div class="col-12 col-sm-6 mb-3 -title">
                     <label class="form-label">大標題 <span class="text-danger">*</span></label>
                     <input class="form-control" name="title" value="{{ old('title', $data->title ?? '') }}" type="text"
-                        placeholder="請輸入大標題" aria-label="大標題" maxlength="12" required>
+                        placeholder="請輸入大標題" aria-label="大標題" maxlength="12" @if ($old_type !== 5) required @endif>
                 </div>
                 {{-- t1, t2 --}}
-                <div class="col-12 col-sm-6 mb-3">
-                    <div class="event_type">
-                        <label class="form-label">商品群組<span class="text-danger">*</span></label>
-                        <select name="group_id" class="form-select" required>
-                            <option value="" @if ('' == old('group_id', $data->group_id ?? '')) selected @endif disabled>請選擇</option>
-                            @foreach ($collectionList as $key => $collection)
-                                <option value="{{ $collection->id }}" @if ($collection->id == old('group_id', $data->group_id ?? '')) selected @endif>
-                                    {{ $collection->name }}</option>
-                            @endforeach
-                        </select>
-                    </div>
+                <div class="col-12 col-sm-6 mb-3 style_type -stype1 -stype2" @if ($old_type !== 1 && $old_type !== 2) hidden @endif>
+                    <label class="form-label">商品群組 <span class="text-danger">*</span></label>
+                    <select name="group_id" class="form-select" required>
+                        <option value="" @if ('' == old('group_id', $data->group_id ?? '')) selected @endif disabled>請選擇</option>
+                        @foreach ($collectionList as $key => $collection)
+                            <option value="{{ $collection->id }}" @if ($collection->id == old('group_id', $data->group_id ?? '')) selected @endif>
+                                {{ $collection->name }}</option>
+                        @endforeach
+                    </select>
                 </div>
-                {{-- t3 --}}
-                <div class="col-12">
+                {{-- t5 --}}
+                <div class="col-12 style_type -stype5"  @if ($old_type !== 5) hidden @endif>
                     <label class="form-label">圖片-群組（最多3個） <span class="text-danger">*</span></label>
                     @php
                         if ($method == 'create') {
@@ -106,7 +107,6 @@
                                 @if (isset($value->file))
                                     {{ $value->file }}
                                 @endif
-
                             </div>
                             <div class="col-12 col-sm-6 mb-1">
                                 <select name="group_id{{ $key }}" class="form-select">
@@ -193,20 +193,29 @@
             // -- 顯示字數 -------------
             showWordsLength($('input[maxlength]'));
 
-            // -- 橫幅廣告類型 -------------
-            $('input[name="event_type"]').on('change', function() {
-                const val = $('input[name="event_type"]:checked').val();
-                $(`div.event_type:not(.-${val})`).prop('hidden', true);
-                $(`div.event_type:not(.-${val})`).children('select, input').prop({
+            // -- 版型 -------------
+            $('input[name="style_type"]').on('change', function() {
+                const val = $('input[name="style_type"]:checked').val();
+                $(`div.style_type`).prop('hidden', true);
+                $(`div.style_type`).children('select, input').prop({
                     'required': false,
                     'disabled': true
                 });
 
-                $(`div.event_type.-${val}`).prop('hidden', false);
-                $(`div.event_type.-${val}`).children('select, input').prop({
+                $(`div.style_type.-stype${val}`).prop('hidden', false);
+                $(`div.style_type.-stype${val}`).children('select, input').prop({
                     'required': true,
                     'disabled': false
                 });
+
+                // 樣式三(5) 標題非必填
+                if (val === '5') {
+                    $('.-title label').html('大標題');
+                    $('.-title input').prop('required', false);
+                } else {
+                    $('.-title label').html('大標題 <span class="text-danger">*</span>');
+                    $('.-title input').prop('required', true);
+                }
             });
         </script>
     @endpush
