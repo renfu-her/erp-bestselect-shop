91d5120822a003ca5eb051c1ace2784f9b87740a
hans <hayashi0126@gmal.com>
2023-03-20 10:42:19 +0800

    ok

--- routes/api/web/OnePage.php
+++ routes/api/web/OnePage.php
@@ -5,6 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'one-page', 'as' => 'one-page'], function () {
     Route::get('get-page/{id}', [OnePageCtrl::class, 'getPage']);
+    Route::post('get-url', [OnePageCtrl::class, 'getUrl']);
 });
-
-
