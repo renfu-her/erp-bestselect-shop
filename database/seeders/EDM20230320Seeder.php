48c0609c1809aca6098220327486dd16ba165d37
08080 <pntcwz@gmail.com>
2023-03-22 09:32:07 +0800

    修改

--- database/seeders/EDM20230320Seeder.php
+++ database/seeders/EDM20230320Seeder.php
@@ -3,13 +3,15 @@
 namespace Database\Seeders;
 
 use App\Enums\Customer\Newsletter;
+use App\Helpers\IttmsUtils;
 use App\Jobs\EDM20230320Job;
+use App\Mail\EDM\EDM20230320;
 use App\Models\Customer;
 use App\Models\MailSendRecord;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class EDM20230320 extends Seeder
+class EDM20230320Seeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -23,16 +25,17 @@ class EDM20230320 extends Seeder
 //        // 發郵件EDM
 //        // 先檢查會員是否有訂閱
 //        $customer = DB::table(app(Customer::class)->getTable(). ' as customers')
-//            ->leftJoin(app(MailSendRecord::class)->getTable(), function ($join) {
+//            ->leftJoin(app(MailSendRecord::class)->getTable(). ' as mail_send_record', function ($join) {
 //                $join->on('customers.email', '=', 'mail_send_record.email')
-//                    ->where('mail_send_record.event', '=', 'EDM20230320');
+//                    ->where('mail_send_record.event', '=', EDM20230320::class );
 //            })
-//            ->where('newsletter', Newsletter::subscribe()->value)
+//            ->where('customers.newsletter', Newsletter::subscribe()->value)
 //            ->whereNull('mail_send_record.id')
 //            ->whereNull('customers.deleted_at')
 //            ->select('customers.email')
-//            ->get();
-//        echo "共有" . count($customer) . "筆資料";
+//            ->get()
+//        ;
+//        echo "共有.." . count($customer) . "筆資料";
 //
 //        for($i = 0; $i < count($customer); $i++) {
 //            //每兩秒執行下一個
