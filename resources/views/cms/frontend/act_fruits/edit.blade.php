eafbe82ef306db24ef158b7bf8540b6e00cf30a5
May.Wang <hsihung08079@gmail.com>
2023-05-26 16:48:12 +0800

    add 水果分類設定

--- resources/views/cms/frontend/act_fruits/edit.blade.php
+++ resources/views/cms/frontend/act_fruits/edit.blade.php
@@ -2,10 +2,10 @@
 @section('sub-content')
     <h2 class="mb-4">
         @if ($method === 'create')
-            新增
+            新增水果
         @else
             編輯
-        @endif 水果
+        @endif
     </h2>
     <form method="post" action="" enctype="multipart/form-data">
         @method('POST')
@@ -17,49 +17,49 @@
                     <label class="form-label">水果名稱 <span class="text-danger">*</span></label>
                     <input class="form-control @error('title') is-invalid @enderror" type="text" 
                         name="title" value=""
-                        placeholder="輸入水果名稱" required aria-label="水果名稱">
+                        placeholder="例：日本水蜜桃蘋果" required aria-label="水果名稱">
                 </div>
                 <div class="col-12 col-sm-6 mb-3">
                     <label class="form-label">副標題 <span class="text-danger">*</span></label>
                     <input class="form-control @error('sub_title') is-invalid @enderror" type="text" 
                         name="sub_title" value=""
-                        placeholder="輸入副標題" required aria-label="副標題">
+                        placeholder="例：數量稀少、多汁爽脆的最佳口味" required aria-label="副標題">
                 </div>
                 <div class="col-12 col-sm-6 mb-3">
                     <label class="form-label">產地 <span class="text-danger">*</span></label>
                     <input class="form-control @error('place') is-invalid @enderror" type="text" 
                         name="place" value=""
-                        placeholder="輸入產地" required aria-label="產地">
+                        placeholder="例：日本青森" required aria-label="產地">
                 </div>
                 <div class="col-12 col-sm-6 mb-3">
                     <label class="form-label">產季 <span class="text-danger">*</span></label>
                     <input class="form-control @error('season') is-invalid @enderror" type="text" 
                         name="season" value=""
-                        placeholder="輸入產季" required aria-label="產季">
+                        placeholder="例：秋季 (10月)" required aria-label="產季">
                 </div>
                 <div class="col-12 mb-3">
                     <label class="form-label">圖片網址 <span class="text-danger">*</span></label>
                     <input class="form-control @error('pic') is-invalid @enderror" type="url" 
                         name="pic" value=""
-                        placeholder="輸入圖片網址" required aria-label="圖片網址">
+                        placeholder="例：https://www.bestselection.com.tw/activity/fruits/image/Fru_sea_05_03.jpg" required aria-label="圖片網址">
                 </div>
                 <div class="col-12 mb-3">
                     <label class="form-label">產品連結 <span class="text-danger">*</span></label>
                     <input class="form-control @error('link') is-invalid @enderror" type="url"
                         name="link" value=""
-                        placeholder="輸入產品連結" required aria-label="產品連結">
+                        placeholder="例：https://www.bestselection.com.tw/product/P230508001?openExternalBrowser=1" required aria-label="產品連結">
                 </div>
                 <div class="col-12 mb-3">
                     <label class="form-label">內文 <span class="text-danger">*</span></label>
                     <textarea name="text" class="form-control @error('text') is-invalid @enderror" 
-                        aria-label="內文"></textarea>
+                        placeholder="例：外表黃綠外帶有一抹腮紅，因此又被暱稱為『水蜜桃蘋果』。" aria-label="內文"></textarea>
                 </div>
                 <div class="col-12 mb-3">
                     <label class="form-label">目前狀態 <span class="text-danger">*</span></label>
                     <select name="status" class="form-select" required>
                         <option value="" selected disabled>請選擇</option>
                         @foreach ($saleStatus as $key => $status)
-                            <option value="{{ $key }}"
+                            <option value="{{ $status }}"
                                 @class(['bg-success' => $key == 0, 'bg-danger' => $key == 13,
                                 'bg-warning' => $key == 14, 'text-white' => $key == 0 || $key == 13])>
                                 {{ $status }}</option>
--- resources/views/cms/frontend/act_fruits/season.blade.php
+++ resources/views/cms/frontend/act_fruits/season.blade.php
@@ -30,80 +30,222 @@
     </li>
 </ul>
 
-<div id="tab1" class="-page">
-    <form action="" method="post">
+<form action="" method="post">
+    <div id="tab1" class="-page">
+        <input type="hidden" name="tab_id[]" value="">
         <div class="card shadow p-4 mb-4">
-            <h6>春季水果 (3至5月)</h6>
-            <div class="col-auto">
-                <button type="submit" class="btn btn-primary px-4">儲存</button>
-            </div>
+            <h6 class="mb-2">春季水果 (3至5月)</h6>
+            <p class="mb-2">拖曳方框以排序
+                <button type="button" class="btn btn-outline-primary border-dashed btn-sm mx-2"
+                    data-bs-toggle="modal" data-bs-target="#addFruit">
+                    <i class="bi bi-plus-circle bold"></i> 加入水果
+                </button>
+            </p>
+            <ul class="sortable mb-3 -serial-number">
+                <li>
+                    <label class="-serial-title -before">茂谷柑</label>
+                    <input type="hidden" name="fruit_[]" value="1">
+                    <span class="icon icon-btn fs-5 text-danger rounded-circle border-0 -del">
+                        <i class="bi bi-trash"></i>
+                    </span>
+                </li>
+                <li>
+                    <label class="-serial-title -before">玉女小番茄</label>
+                    <input type="hidden" name="fruit_[]" value="2">
+                    <span class="icon icon-btn fs-5 text-danger rounded-circle border-0 -del">
+                        <i class="bi bi-trash"></i>
+                    </span>
+                </li>
+                <li>
+                    <label class="-serial-title -before">橙蜜香小番茄</label>
+                    <input type="hidden" name="fruit_[]" value="3">
+                    <span class="icon icon-btn fs-5 text-danger rounded-circle border-0 -del">
+                        <i class="bi bi-trash"></i>
+                    </span>
+                </li>
+                <li>
+                    <label class="-serial-title -before">鳳梨釋迦</label>
+                    <input type="hidden" name="fruit_[]" value="4">
+                    <span class="icon icon-btn fs-5 text-danger rounded-circle border-0 -del">
+                        <i class="bi bi-trash"></i>
+                    </span>
+                </li>
+            </ul>
         </div>
-    </form>
-</div>
-<div id="tab2" class="-page" hidden>
-    <form action="" method="post">
+    </div>
+    <div id="tab2" class="-page" hidden>
+        <input type="hidden" name="tab_id[]" value="">
         <div class="card shadow p-4 mb-4">
-            <h6>夏季水果 (6至8月)</h6>
-            <div class="col-auto">
-                <button type="submit" class="btn btn-primary px-4">儲存</button>
-            </div>
+            <h6 class="mb-2">夏季水果 (6至8月)</h6>
+            <p class="mb-2">拖曳方框以排序
+                <button type="button" class="btn btn-outline-primary border-dashed btn-sm mx-2"
+                    data-bs-toggle="modal" data-bs-target="#addFruit">
+                    <i class="bi bi-plus-circle bold"></i> 加入水果
+                </button>
+            </p>
+            <ul class="sortable mb-3">
+            </ul>
         </div>
-    </form>
-</div>
-<div id="tab3" class="-page" hidden>
-    <form action="" method="post">
+    </div>
+    <div id="tab3" class="-page" hidden>
+        <input type="hidden" name="tab_id[]" value="">
         <div class="card shadow p-4 mb-4">
-            <h6>秋季水果 (9至11月)</h6>
-            <div class="col-auto">
-                <button type="submit" class="btn btn-primary px-4">儲存</button>
-            </div>
+            <h6 class="mb-2">秋季水果 (9至11月)</h6>
+            <p class="mb-2">拖曳方框以排序
+                <button type="button" class="btn btn-outline-primary border-dashed btn-sm mx-2"
+                    data-bs-toggle="modal" data-bs-target="#addFruit">
+                    <i class="bi bi-plus-circle bold"></i> 加入水果
+                </button>
+            </p>
+            <ul class="sortable mb-3">
+            </ul>
         </div>
-    </form>
-</div>
-<div id="tab4" class="-page" hidden>
-    <form action="" method="post">
+    </div>
+    <div id="tab4" class="-page" hidden>
+        <input type="hidden" name="tab_id[]" value="">
         <div class="card shadow p-4 mb-4">
-            <h6>冬季水果 (12至2月)</h6>
-            <div class="col-auto">
-                <button type="submit" class="btn btn-primary px-4">儲存</button>
-            </div>
+            <h6 class="mb-2">冬季水果 (12至2月)</h6>
+            <p class="mb-2">拖曳方框以排序
+                <button type="button" class="btn btn-outline-primary border-dashed btn-sm mx-2"
+                    data-bs-toggle="modal" data-bs-target="#addFruit">
+                    <i class="bi bi-plus-circle bold"></i> 加入水果
+                </button>
+            </p>
+            <ul class="sortable mb-3">
+            </ul>
         </div>
-    </form>
-</div>
-<div id="tab5" class="-page" hidden>
-    <form action="" method="post">
+    </div>
+    <div id="tab5" class="-page" hidden>
+        <input type="hidden" name="tab_id[]" value="">
         <div class="card shadow p-4 mb-4">
-            <h6>進口水果 (1至12月)</h6>
-            <div class="col-auto">
-                <button type="submit" class="btn btn-primary px-4">儲存</button>
-            </div>
+            <h6 class="mb-2">進口水果 (1至12月)</h6>
+            <p class="mb-2">拖曳方框以排序
+                <button type="button" class="btn btn-outline-primary border-dashed btn-sm mx-2"
+                    data-bs-toggle="modal" data-bs-target="#addFruit">
+                    <i class="bi bi-plus-circle bold"></i> 加入水果
+                </button>
+            </p>
+            <ul class="sortable mb-3">
+                <li>
+                    <label class="-serial-title -before">日本水蜜桃</label>
+                    <input type="hidden" name="fruit_[]" value="27">
+                    <span class="icon icon-btn fs-5 text-danger rounded-circle border-0 -del">
+                        <i class="bi bi-trash"></i>
+                    </span>
+                </li>
+                <li>
+                    <label class="-serial-title -before">日本麝香葡萄</label>
+                    <input type="hidden" name="fruit_[]" value="28">
+                    <span class="icon icon-btn fs-5 text-danger rounded-circle border-0 -del">
+                        <i class="bi bi-trash"></i>
+                    </span>
+                </li>
+                <li>
+                    <label class="-serial-title -before">日本水蜜桃蘋果</label>
+                    <input type="hidden" name="fruit_[]" value="29">
+                    <span class="icon icon-btn fs-5 text-danger rounded-circle border-0 -del">
+                        <i class="bi bi-trash"></i>
+                    </span>
+                </li>
+                <li>
+                    <label class="-serial-title -before">日本陽光蜜富士</label>
+                    <input type="hidden" name="fruit_[]" value="30">
+                    <span class="icon icon-btn fs-5 text-danger rounded-circle border-0 -del">
+                        <i class="bi bi-trash"></i>
+                    </span>
+                </li>
+                <li>
+                    <label class="-serial-title -before">秘魯無籽葡萄</label>
+                    <input type="hidden" name="fruit_[]" value="31">
+                    <span class="icon icon-btn fs-5 text-danger rounded-circle border-0 -del">
+                        <i class="bi bi-trash"></i>
+                    </span>
+                </li>
+            </ul>
         </div>
-    </form>
-</div>
+    </div>
 
-<div class="col-auto">
-    <a href="{{ Route('cms.act-fruits.index') }}" class="btn btn-outline-primary px-4"
-        role="button">返回列表</a>
-</div>
+    <div class="col-auto">
+        <button type="submit" class="btn btn-primary px-4">儲存</button>
+        <a href="{{ Route('cms.act-fruits.index') }}" class="btn btn-outline-primary px-4"
+            role="button">返回列表</a>
+    </div>
+</form>
+
+<x-b-modal id="addFruit" cancelBtn="false" size="modal-lg">
+    <x-slot name="title">選取水果</x-slot>
+    <x-slot name="body">
+        <div class="row">
+            <div class="col-12">
+                <select multiple class="-select2 -multiple form-select">
+                    <option value="1">茂谷柑</option>
+                    <option value="2">玉女小番茄</option>
+                    <option value="3">橙蜜香小番茄</option>
+                    <option value="4">鳳梨釋迦</option>
+                    <option value="5">大目釋迦</option>
+                    <option value="6">牛奶蜜棗</option>
+                    <option value="7">貴妃枇杷</option>
+                    <option value="8">玉荷包</option>
+                    <option value="27">日本水蜜桃</option>
+                    <option value="28">日本麝香葡萄</option>
+                    <option value="29">日本水蜜桃蘋果</option>
+                    <option value="30">日本陽光蜜富士</option>
+                    <option value="31">秘魯無籽葡萄</option>
+                </select>
+            </div>
+        </div>
+    </x-slot>
+    <x-slot name="foot">
+        <button type="button" class="btn btn-primary btn-ok">加入分類</button>
+    </x-slot>
+</x-b-modal>
 @endsection
 
 @once
     @push('sub-styles')
         <style>
-            /* 拖曳預覽框 */
-            .-appendClone.--selectedP tr.placeholder-highlight {
-                width: 100%;
-                height: 60px;
-                margin-bottom: .5rem;
-                display: table-row;
+            /* 拖曳塊 */
+            .sortable {
+                list-style-type: none;
+                margin: 0;
+                padding: 0;
+                position: relative;
+                display: flex;
+                flex-wrap: wrap;
+                justify-content: space-between;
             }
-            tr.placeholder-highlight > td {
-                border: none;
+            .sortable li {
+                margin: 10px;
+                padding: 5px 10px;
+                background-color: var(--bs-light);
+                box-shadow: 0 0 0 2px var(--bs-green);
+                border-radius: 10px;
+                display: flex;
+                align-items: center;
+                width: calc(50% - 20px);
+                cursor: move;
+            }
+            .sortable li label {
+                flex: 1;
+                cursor: move;
+            }
+            @media screen and (max-width: 768px) {
+                .sortable li {
+                    width: 100%;
+                }
+            }
+            /* 拖曳預覽框 */
+            .sortable li.placeholder-highlight {
+                height: 50px;
+                box-shadow: none;
+                background-color: #ffe00030;
             }
         </style>
     @endpush
     @push('sub-scripts')
         <script>
+            delFn();
+            // Tabs
             $('.nav-link').off('click').on('click', function() {
                 const $this = $(this);
                 const page = $this.data('page');
@@ -115,6 +257,65 @@
                 $('.-page').prop('hidden', true);
                 $(`#${page}`).prop('hidden', false);
             });
+
+            // sortable
+            $('.sortable').sortable({
+                placeholder: 'placeholder-highlight',
+                cursor: 'move'
+            });
+
+            // 加入水果Modal
+            const addFruitModal = new bootstrap.Modal(document.getElementById('addFruit'));
+            // -- 開啟 Modal
+            $('#addFruit').on('show.bs.modal', function (e) {
+                // console.log(e.relatedTarget);
+                const $page = $(e.relatedTarget).closest('.-page');
+                let id = [];
+                $('input[name^="fruit_"]', $page).each(function (index, element) {
+                    // element == this
+                    id.push($(element).val());
+                });
+                $('#addFruit select.-select2').val(id);
+                $('#addFruit select.-select2').trigger('change');
+                $('#addFruit .btn-ok').data('tab', $page.attr('id'));
+            });
+            // -- 加入
+            $('#addFruit .btn-ok').off('click.add').on('click.add', function () {
+                const tab = $(this).data('tab');
+                const $page = $(`#${tab}`);
+                const $ul = $('ul.sortable', $page);
+                const _newFruits = $('#addFruit select.-select2').select2('data');
+                $('.sortable li', $page).addClass('del');
+
+                // $('#addFruit select.-select2 option:selected').each(function (index, element) {});
+                _newFruits.forEach(data => {
+                    if ($(`input[name^="fruit_"][value="${data.id}"]`, $page).length > 0) {
+                        $(`.sortable li:has(input[name^="fruit_"][value="${data.id}"])`, $page).removeClass('del');
+                    } else {
+                        $ul.append(`
+                            <li>
+                                <label class="-serial-title -before">${data.text}</label>
+                                <input type="hidden" name^="fruit_" value="${data.id}">
+                                <span class="icon icon-btn fs-5 text-danger rounded-circle border-0 -del">
+                                    <i class="bi bi-trash"></i>
+                                </span>
+                            </li>
+                        `);
+                    }
+                });
+                
+                // 刪除
+                $('.sortable li.del', $page).remove();
+                delFn($page);
+                // 關閉懸浮視窗
+                addFruitModal.hide();
+            });
+            // -- 刪除 .-del
+            function delFn(page = '') {
+                $('.-del', page).off('click').on('click', function () {
+                    $(this).closest('li').remove();
+                });
+            }
         </script>
     @endpush
 @endOnce
