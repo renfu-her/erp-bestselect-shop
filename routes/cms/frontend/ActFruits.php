9b84de3cc49cd10841cbee6b930d1dd7f41ff86f
hans <hayashi0126@gmal.com>
2023-05-30 09:31:21 +0800

    ok

--- routes/cms/frontend/ActFruits.php
+++ routes/cms/frontend/ActFruits.php
@@ -5,13 +5,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'act-fruits', 'as' => 'act-fruits.'], function () {
     Route::get('', [ActFruitsCtrl::class, 'index'])->name('index')->middleware('permission:cms.act-fruits.index');
-    Route::get('edit/{id}', [ActFruitsCtrl::class, 'edit'])->name('edit')->middleware('permission:cms.act-fruits.edit');
+    Route::get('edit/{id}', [ActFruitsCtrl::class, 'edit'])->name('edit');
     Route::post('edit/{id}', [ActFruitsCtrl::class, 'update']);
-    Route::get('create', [ActFruitsCtrl::class, 'create'])->name('create')->middleware('permission:cms.act-fruits.create');
+    Route::get('create', [ActFruitsCtrl::class, 'create'])->name('create');
     Route::post('create', [ActFruitsCtrl::class, 'store']);
-    Route::get('delete/{id}', [ActFruitsCtrl::class, 'destroy'])->name('delete')->middleware('permission:cms.act-fruits.delete');
+    Route::get('delete/{id}', [ActFruitsCtrl::class, 'destroy'])->name('delete');
 
-    Route::get('season', [ActFruitsCtrl::class, 'season'])->name('season')->middleware('permission:cms.act-fruits.season');
+    Route::get('season', [ActFruitsCtrl::class, 'season'])->name('season');
     Route::post('season', [ActFruitsCtrl::class, 'seasonUpdate']);
 
 });
