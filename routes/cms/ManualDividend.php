22729d154706e1d0f57bff9889f59fd32ce2e8f6
hans <hayashi0126@gmal.com>
2023-06-07 15:24:13 +0800

    ok

--- routes/cms/ManualDividend.php
+++ routes/cms/ManualDividend.php
@@ -1,11 +1,11 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Cms\Marketing\ManualDividendCtrl;
+use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'manual-dividend', 'as' => 'manual-dividend.'], function () {
-    Route::get('', [ManualDividendCtrl::class, 'index'])->name('index')->middleware('permission:cms.manual-dividend.index');
-    Route::get('create', [ManualDividendCtrl::class, 'create'])->name('create')->middleware('permission:cms.manual-dividend.create');
+Route::group(['prefix' => 'manual-dividend', 'as' => 'manual-dividend.', 'middleware' => ['permission:cms.manual-dividend.index']], function () {
+    Route::get('', [ManualDividendCtrl::class, 'index'])->name('index');
+    Route::get('create', [ManualDividendCtrl::class, 'create'])->name('create');
     Route::post('create', [ManualDividendCtrl::class, 'store']);
     Route::get('show/{id}', [ManualDividendCtrl::class, 'show'])->name('show');
 
