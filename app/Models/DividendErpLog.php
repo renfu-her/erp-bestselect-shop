422148ee617992ffd36fdc3d70f524a4febaa1e0
hans <hayashi0126@gmal.com>
2023-06-13 10:07:35 +0800

    ok

---
+++ app/Models/DividendErpLog.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DividendErpLog extends Model
+{
+    use HasFactory;
+    protected $table = 'dis_dividend_erp_log';
+    protected $guarded = [];
+}
