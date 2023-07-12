bf19bfdd53f8367af84fbcb07a53735e2b2443b2
08114 <domosn1017@gmail.com>
2023-07-12 15:19:14 +0800

    # adj

--- app/Http/Controllers/Cms/AdminManagement/RefExpenditurePetitionCtrl.php
+++ app/Http/Controllers/Cms/AdminManagement/RefExpenditurePetitionCtrl.php
@@ -25,7 +25,7 @@ class RefExpenditurePetitionCtrl extends Controller
             $order = isset($d['order']) ? $d['order'] : [];
             $order = array_values(array_filter($order));
 
-            $o_order = DB::table('pet_order_sn')
+            $old_p_e = DB::table('pet_order_sn')
                 ->leftJoin('pet_petition as pet', function ($join) {
                     $join->on('pet.id', '=', 'pet_order_sn.source_id')
                         ->where('pet_order_sn.source_type', '=', 'petition');
@@ -35,23 +35,41 @@ class RefExpenditurePetitionCtrl extends Controller
                         ->where('pet_order_sn.source_type', '=', 'expenditure');
                 })
                 ->where('pet_order_sn.order_sn', $current_sn)
-                ->select('pet_order_sn.id', DB::raw('IF(pet.sn IS NULL, exp.sn, pet.sn) AS order_sn'))
+                ->select('pet_order_sn.source_type', 'pet_order_sn.source_id')
                 ->get();
-
-            $dArray = array_diff($o_order->pluck('order_sn', 'id')->toArray(), $order);
-            if($dArray) DB::table('pet_order_sn')->whereIn('id', array_flip($dArray))->delete();
+            foreach ($old_p_e as $ope_value) {
+                DB::table('pet_order_sn')
+                ->where('source_id', $ope_value->source_id,)
+                ->where('source_type', $ope_value->source_type)
+                ->delete();
+            }
 
             $err = false;
             $errors = [];
-            foreach($order as $key => $value){
-                $re = Petition::reverseBind($current_sn, $value);
+            $new_p_e = array_filter($order, function($v){
+                return (substr($v, 0, 3) == 'PET' || substr($v, 0, 3) == 'EXP');
+            });
+            foreach($new_p_e as $n_p_e_key => $n_p_e_value){
+                $re = Petition::reverseBind($current_sn, $n_p_e_value);
 
                 if ($re['success'] != '1') {
                     $err = true;
-                    $errors['order.' . $key] = $re['message'];
+                    $errors['order.' . $n_p_e_key] = $re['message'];
                 }
             }
+            $new_other = array_filter($order, function($v) {
+                return (substr($v, 0, 3) != 'PET' && substr($v, 0, 3) != 'EXP');
+            });
+            foreach($new_other as $n_o_key => $n_o_value){
+                foreach($new_p_e as $npe_value){
+                    $res = Petition::reverseBind($n_o_value, $npe_value);
 
+                    if ($res['success'] != '1') {
+                        $err = true;
+                        $errors['order.' . $n_o_key] = $res['message'];
+                    }
+                }
+            }
             if($err){
                 DB::rollBack();
                 return redirect()->back()->withInput($d)->withErrors($errors);
@@ -78,10 +96,25 @@ class RefExpenditurePetitionCtrl extends Controller
                 ->get()->toArray()
             );
 
+        $current_p_e = DB::table('pet_order_sn')
+            ->where('order_sn', $current_sn)
+            ->select('source_type', 'source_id')
+            ->get()->toArray();
+
+        foreach ($current_p_e as $value) {
+            $orders = array_merge($orders, DB::table('pet_order_sn')
+                ->where('source_type', $value->source_type)
+                ->where('source_id', $value->source_id)
+                ->where('order_sn', '!=', $current_sn)
+                ->pluck('order_sn')
+                ->toArray()
+            );
+        }
+
         return view('cms.admin_management.ref_expenditure_petition.edit', [
             'form_action'=>route('cms.ref_expenditure_petition.edit', ['current_sn' => $current_sn]),
             'back_url'=>url()->previous(),
-            'order' => $orders,
+            'order' => array_unique($orders),
         ]);
     }
 }
