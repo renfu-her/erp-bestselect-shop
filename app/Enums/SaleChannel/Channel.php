e6514c1f3dc8abcc9e2d6615d62723d04404f368
Yoyo <conan0828+gitlab@gmail.com>
2023-09-15 16:55:56 +0800

    通路價格只顯示以下的通路

--- app/Enums/SaleChannel/Channel.php
+++ app/Enums/SaleChannel/Channel.php
@@ -10,6 +10,10 @@ use BenSampo\Enum\Enum;
  * @method static static B2b()
  * @method static static Shopee()
  * @method static static Post()
+ * @method static static DepartureLounge() 喜鴻餐飲
+ * @method static static Besttour() 喜鴻旅行社
+ * @method static static DealerPriceNoBonus() 經銷價販售-無獎金
+ * @method static static MultipleOrders() 大量訂購通路
  */
 final class Channel extends Enum
 {
@@ -18,6 +22,10 @@ final class Channel extends Enum
     const B2b = 3;
     const Shopee = 4;
     const Post = 5;
+    const DepartureLounge = 8;
+    const Besttour = 9;
+    const DealerPriceNoBonus = 10;
+    const MultipleOrders = 16;
 
     public static function getDescription($value): string
     {
