a3b0d721620bcfa5e79eecadc6cb0b6f93a4e8bc
08080 <chihguwang@besttour.com.tw>
2025-04-16 15:00:46 +0800

    修改，嘗試讓開發機可跑

--- app/Helpers/auth_mpi_mac.php
+++ app/Helpers/auth_mpi_mac.php
@@ -236,7 +236,7 @@ function opensslDecrypt($cipher_text, $key, $iv) {
     return $plain_text;
 }
 function pkcs5Unpad($text) {
-    $pad = ord($text{strlen($text) - 1});
+    $pad = ord($text[strlen($text) - 1]);
     if ($pad > strlen($text)) return false;
     if (strspn($text, chr($pad), strlen($text) - $pad) != $pad) return false;
     return substr($text, 0, -1 * $pad);
